cdc0b0ba3abf96496b41205dd543c577ce4fb1ef
gentlegiantJGC <james_clare1@yahoo.co.uk>
2018-12-17 15:12:03 +0000

    turned Primitives into a package so that each block isn't re-evaluated each time

---
+++ Primitives/__init__.py
@@ -0,0 +1,34 @@
+import json
+import os
+from .scripts import *
+
+print('Loading Primitives')
+
+
+def load_file(path: str) -> dict:
+	with open(path) as f_:
+		if path.endswith('.json'):
+			return json.load(f_)
+		elif path.endswith('.pyjson'):
+			return eval(f_.read())
+		else:
+			print(f'Could not load {path}')
+
+
+blocks = {'numerical': {}}
+
+for start_folder in blocks:
+	for root, dirs, files in os.walk(f'{os.path.dirname(__file__)}/{start_folder}'):
+		for f in files:
+			if os.path.splitext(f)[0] in blocks[start_folder]:
+				print(f'Block name "{os.path.splitext(f)[0]}" is define twice')
+			try:
+				blocks[start_folder][os.path.splitext(f)[0]] = load_file(f'{root}/{f}')
+			except Exception as e:
+				print(f'Failed to load {root}/{f}\n{e}')
+
+
+def get_block(block_format: str, block_name: str) -> dict:
+	assert block_format in blocks
+	assert block_name in blocks[block_format]
+	return blocks[block_format][block_name]
