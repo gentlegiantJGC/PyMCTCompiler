fed8a33083b78b02982d09b37a6b7483578bcfad
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-01-19 10:46:27 +0000

    various modifications to the read script

--- Reader/read.py
+++ Reader/read.py
@@ -78,11 +78,11 @@ class VersionContainer:
 		assert platform in self.versions and version_number in self.versions[platform]
 		return self.versions[platform][version_number]
 
-	def to_universal(self, platform: str, version_number: Tuple[int, int, int], namespace: str, block_id: str, properties: Dict[str, str], force_blockstate: bool = False):
-		return self.get_version(platform, version_number).to_universal(namespace, block_id, properties, force_blockstate)
+	def to_universal(self, level, platform: str, version_number: Tuple[int, int, int], namespace: str, block_id: str, properties: Dict[str, str], force_blockstate: bool = False):
+		return self.get_version(platform, version_number).to_universal(level, namespace, block_id, properties, force_blockstate)
 
-	def from_universal(self, platform: str, version_number: Tuple[int, int, int], namespace: str, block_id: str, properties: Dict[str, str], force_blockstate: bool = False):
-		return self.get_version(platform, version_number).from_universal(namespace, block_id, properties, force_blockstate)
+	def from_universal(self, level, platform: str, version_number: Tuple[int, int, int], namespace: str, block_id: str, properties: Dict[str, str], force_blockstate: bool = False):
+		return self.get_version(platform, version_number).from_universal(level, namespace, block_id, properties, force_blockstate)
 
 
 class Version:
@@ -101,20 +101,22 @@ class Version:
 			self._format = init_file["format"]
 
 			self._subversions = {}
+			self._numerical_map = None
+			self._numerical_map_inverse = None
 
-			if init_file["format"] in ["numerical", "pseudo-numerical"]:
+			if self.format in ["numerical", "pseudo-numerical"]:
 				for block_format in ["blockstate", "numerical"]:
 					self._subversions[block_format] = SubVersion(f'{version_path}/{block_format}', version_container)
+				if self.format == "numerical":
+					with open(f'{version_path}/__numerical_map__.json') as f:
+						self._numerical_map = json.load(f)
+					self._numerical_map_inverse = {}
+					for block_id, block_string in self._numerical_map.items():
+						assert isinstance(block_id, str) and isinstance(block_string, str) and block_id.isnumeric()
+						self._numerical_map_inverse[block_string] = block_id
 
-			elif init_file["format"] == "blockstate":
+			elif self.format == "blockstate":
 				self._subversions["blockstate"] = SubVersion(version_path, version_container)
-
-
-
-				# with open(f'{mappings_path}/{_version}/__numerical_map__.json') as f:
-				# 	numerical_map = json.load(f)
-				# for block_id, block_string in numerical_map.items():
-				# 	print('hi')
 
 	@property
 	def format(self) -> str:
@@ -137,11 +139,17 @@ class Version:
 			elif self.format == "blockstate":
 				return self._subversions["blockstate"]
 
-	def to_universal(self, namespace: str, block_name: str, properties: Dict[str, str], force_blockstate: bool = False):
-		return self.get_format(force_blockstate).to_universal(namespace, block_name, properties)
+	def to_universal(self, level, namespace: str, block_name: str, properties: Dict[str, str], force_blockstate: bool = False):
+		if self.format == "numerical" and not force_blockstate:
+			namespace, block_name = self._numerical_map[block_name].split(':')
+		blockstate = self.get_format(force_blockstate).to_universal(level, namespace, block_name, properties)
+		return blockstate
 
-	def from_universal(self, namespace: str, block_name: str, properties: Dict[str, str], force_blockstate: bool = False):
-		return self.get_format(force_blockstate).from_universal(namespace, block_name, properties)
+	def from_universal(self, level, namespace: str, block_name: str, properties: Dict[str, str], force_blockstate: bool = False):
+		blockstate = self.get_format(force_blockstate).from_universal(level, namespace, block_name, properties)
+		if self.format == "numerical":
+			blockstate["block_name"] = self._numerical_map_inverse[blockstate["block_name"]]
+		return blockstate
 
 
 class SubVersion:
@@ -155,16 +163,16 @@ class SubVersion:
 		for namespace in directories(sub_version_path):
 			self.namespaces[namespace] = Namespace(f'{sub_version_path}/{namespace}', namespace, version_container, self)
 
-	def to_universal(self, namespace: str, block_name: str, properties: Dict[str, str]):
+	def to_universal(self, level, namespace: str, block_name: str, properties: Dict[str, str]):
 		try:
-			return self.get_namespace(namespace).to_universal(block_name, properties)
+			return self.get_namespace(namespace).to_universal(level, block_name, properties)
 		except Exception as e:
 			print(f'Failed getting namespace. It may not exist.\n{e}')
 			return {"block_name": f"{namespace}/{block_name}", "properties": properties}
 
-	def from_universal(self, namespace: str, block_name: str, properties: Dict[str, str]):
+	def from_universal(self, level, namespace: str, block_name: str, properties: Dict[str, str]):
 		try:
-			return self.get_namespace(namespace).from_universal(block_name, properties)
+			return self.get_namespace(namespace).from_universal(level, block_name, properties)
 		except Exception as e:
 			print(f'Failed getting namespace. It may not exist.\n{e}')
 			return {"block_name": f"{namespace}/{block_name}", "properties": properties}
@@ -203,22 +211,28 @@ class Namespace:
 	def get_mapping_from_universal(self, block_name):
 		return self._blocks["from_universal"][block_name]
 
-	def to_universal(self, block_name: str, properties: Dict[str, str]):
+	def to_universal(self, level, block_name: str, properties: Dict[str, str]):
 		blockstate = {"block_name": f"{self.namespace}/{block_name}", "properties": properties}
 		try:
-			return self.convert(
+			blockstate = self.convert(
+				level,
 				blockstate,
 				self._blocks["to_universal"][block_name],
 				self.version_container.get_version('universal', (1, 0, 0)).get_format()
 			)
+			if blockstate["nbt"] != {}:
+				print(f'universal mappings should not have nbt: {blockstate}')
+			del blockstate["nbt"]
+
 		except Exception as e:
 			print(f'Failed converting blockstate to universal\n{e}')
-			return blockstate
+		return blockstate
 
-	def from_universal(self, block_name: str, properties: Dict[str, str]):
+	def from_universal(self, level, block_name: str, properties: Dict[str, str]):
 		blockstate = {"block_name": f"{self.namespace}/{block_name}", "properties": properties}
 		try:
 			return self.convert(
+				level,
 				blockstate,
 				self._blocks["from_universal"][block_name],
 				self.current_version
@@ -227,64 +241,80 @@ class Namespace:
 			print(f'Failed converting blockstate from universal\n{e}')
 			return blockstate
 
-	def convert(self, input_blockstate: dict, mappings: dict, output_version: SubVersion):
+	def convert(self, level, input_blockstate: dict, mappings: dict, output_version: SubVersion):
 		"""
 			A demonstration function on how to read the json files to convert into or out of the numerical format
 			You should implement something along these lines into you own code if you want to read them.
 
+			:param level: a view into the level to access additional data
 			:param input_blockstate: the blockstate put into the converter eg {"block_name": "minecraft:log", "properties": {"block_data": "0"}}
 			:param mappings: the mapping file for that block
 			:param output_version: A way for the function to look at the specification being converted to. (used to load default properties)
 			:return: The converted blockstate
 		"""
-		output_blockstate, temp_properties = self._convert(input_blockstate, mappings, output_version)
-		for key, val in temp_properties.items():
-			output_blockstate['properties'][key] = val
+		output_blockstate, new = self._convert(level, input_blockstate, mappings, output_version)
+		for entry in ["properties", "nbt"]:
+			for key, val in new[entry].items():
+				output_blockstate[entry][key] = val
 		return output_blockstate
 
-	def _convert(self, input_blockstate: dict, mappings: dict, output_version: SubVersion):
-		output_blockstate = {"block_name": None, "properties": {}}
-		new_properties = {}  # There could be multiple "new_block" functions in the mappings so new properties are put in here and merged at the very end
+	def _convert(self, level, input_blockstate: dict, mappings: dict, output_version: SubVersion):
+		output_blockstate = {"block_name": None, "properties": {}, "nbt": {}}
+		new = {"properties": {}, "nbt": {}}  # There could be multiple "new_block" functions in the mappings so new properties are put in here and merged at the very end
 		if "new_block" in mappings:
 			assert isinstance(mappings["new_block"], str)
 			output_blockstate["block_name"] = mappings["new_block"]
 			namespace, block_name = output_blockstate["block_name"].split(':')
 			output_blockstate["properties"] = output_version.get_namespace(namespace).get_specification(block_name).get("defaults", {})
 
+		"multiblock"
+
 		if "map_properties" in mappings:
 			for key in mappings["map_properties"]:
 				if key in input_blockstate["properties"]:
 					val = input_blockstate["properties"][key]
 					if val in mappings["map_properties"][key]:
-						temp_blockstate, temp_properties = self._convert(input_blockstate, mappings["map_properties"][key][val], output_version)
+						temp_blockstate, temp_new = self._convert(level, input_blockstate, mappings["map_properties"][key][val], output_version)
 						if temp_blockstate["block_name"] is not None:
 							output_blockstate = temp_blockstate
-						for key2, val2 in temp_properties.items():
-							new_properties[key2] = val2
+						for entry in ["properties", "nbt"]:
+							for key2, val2 in temp_new[entry].items():
+								new[entry][key2] = val2
 					else:
 						raise Exception(f'Value "{val}" for property "{key}" is not present in the mappings')
 				else:
 					raise Exception(f'Property "{key}" is not present in the input blockstate')
 
+		"map_block_name"
+		"map_nbt"
+
 		if "new_properties" in mappings:
 			for key, val in mappings["new_properties"].items():
-				new_properties[key] = val
+				new["properties"][key] = val
+
+		"new_nbt"
 
 		if "carry_properties" in mappings:
 			for key in mappings["carry_properties"]:
 				if key in input_blockstate["properties"]:
 					val = input_blockstate["properties"][key]
 					if val in mappings["carry_properties"][key]:
-						new_properties[key] = val
+						new["properties"][key] = val
 				else:
 					raise Exception(f'Property "{key}" is not present in the input blockstate')
 
-		return output_blockstate, new_properties
+		return output_blockstate, new
 
 
 if __name__ == "__main__":
 	block_mappings = VersionContainer(r"..\versions")
+	print('==== bedrock_1_7_0 ====')
 	for data in range(16):
 		print(
-			block_mappings.to_universal('bedrock', (1, 7, 0), 'minecraft', 'log', {"block_data": str(data)})
+			block_mappings.to_universal(None, 'bedrock', (1, 7, 0), 'minecraft', 'log', {"block_data": str(data)})
+		)
+	print('==== java_1_12_2 ====')
+	for data in range(16):
+		print(
+			block_mappings.to_universal(None, 'java', (1, 12, 2), 'minecraft', '17', {"block_data": str(data)})
 		)
