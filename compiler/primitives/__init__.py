ef29c7f5bd77dfca6f14bb47484758b697a3d8f2
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-03-12 18:06:36 +0000

    added more debugging in primitives

--- compiler/primitives/__init__.py
+++ compiler/primitives/__init__.py
@@ -3,8 +3,6 @@ import os
 import traceback
 from .scripts import *
 
-print('Loading Primitives')
-
 
 def load_file(path: str) -> dict:
 	with open(path) as f_:
@@ -13,9 +11,10 @@ def load_file(path: str) -> dict:
 		elif path.endswith('.pyjson'):
 			return eval(f_.read())
 		else:
-			print(f'Could not load {path}')
+			print(f'Could not load {path}. Not a .json or .pyjson file')
 
 
+print('Loading Primitives')
 blocks = {'numerical': {}}
 
 for start_folder in blocks:
@@ -30,7 +29,10 @@ for start_folder in blocks:
 				print(traceback.print_tb(e.__traceback__))
 
 
+print('Finished Loading Primitives')
+
+
 def get_block(block_format: str, block_name: str) -> dict:
-	assert block_format in blocks
-	assert block_name in blocks[block_format]
+	assert block_format in blocks, f'"{block_format}" is not a known format'
+	assert block_name in blocks[block_format], f'"{block_name}" is not present in the mappings for format "{block_format}"'
 	return blocks[block_format][block_name]
