79af390f4752a4dce37869977b047c1949bbec6e
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-03-19 16:41:43 +0000

    Modified the tests to work with the new reader

--- tests/in_and_out_test.py
+++ tests/in_and_out_test.py
@@ -1,42 +1,42 @@
-from reader.read import VersionContainer
+from reader.read import VersionContainer, Block
 import itertools
 
 if __name__ == '__main__':
-	block_mappings = VersionContainer(r'..\versions')
-	for platform_name in block_mappings.platforms:
-		for version_number in block_mappings.version_numbers(platform_name):
-			version = block_mappings.get(platform_name, version_number)
+	mappings = VersionContainer(r'..\mappings')
+	for platform_name in mappings.platforms:
+		for version_number in mappings.version_numbers(platform_name):
+			version = mappings.get(platform_name, version_number)
 			if not version.format == 'pseudo-numerical':
 				print(f'skipping {platform_name} {version_number}. Not pseudo-numerical format')
 				continue
 			input_version = version.get()
 			for namespace_str in input_version.namespaces:
-				namespace = input_version.get(namespace_str)
-				for block_name in namespace.block_names:
-					block_specification = namespace.get_specification(block_name)
+				for block_name in input_version.block_names(namespace_str):
+					block_specification = input_version.get_specification('block', namespace_str, block_name)
 					properties = block_specification.get('properties', {})
 					keys, values = zip(*properties.items())
 					for spec_ in itertools.product(*values):
 						spec = dict(zip(keys, spec_))
+						input_block = Block(namespace_str, block_name, spec)
 						try:
-							universal_blockstate, extra = namespace.to_universal(None, block_name, spec)
+							output, extra_output, extra_needed = input_version.to_universal(None, input_block)
 						except:
 							print('error to universal')
 							print({'block_name': f'{namespace_str}:{block_name}', 'properties': spec})
 							continue
-						if extra:
+						if extra_needed or extra_output is not None:
 							print(f'skipping {platform_name} {version_number} {namespace_str} {block_name} {spec}. Needs more data')
 							continue
 						try:
-							back_out = block_mappings.from_universal(None, platform_name, version_number, *universal_blockstate['block_name'].split(':'), universal_blockstate['properties'])
+							back_out = input_version.from_universal(None, output)
 						except:
 							print('error from universal')
-							print(universal_blockstate)
+							print(output)
 							print({'block_name': f'{namespace_str}:{block_name}', 'properties': spec})
 							continue
-						if not back_out['block_name'] == f'{namespace_str}:{block_name}' and back_out['properties'] == spec:
-							print(f"Conversion error: {{'{block_name}': '{namespace_str}:{block_name}', 'properties': {spec}}} != {back_out}")
-							print(f'Universal: {universal_blockstate}')
+						if str(input_block) != str(back_out[0]):
+							print(f"Conversion error: {{'{block_name}': '{namespace_str}:{block_name}', 'properties': {spec}}} != {back_out[0]}")
+							print(f'Universal: {output}')
 
 
 
