5837277882ecc00b7c6b9ca306d8a932d0794483
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-03-29 16:39:01 +0000

    [Primitives] Extended blockstate colour function to carry extra

--- compiler/primitives/scripts/blockstate.py
+++ compiler/primitives/scripts/blockstate.py
@@ -28,30 +28,53 @@ def stone(input_namespace: str, input_block_name: str, polished: bool, universal
 	}
 
 
-def colour(input_namespace: str, input_block_name: str, colour: str, universal_namespace: str = None, universal_block_name: str = None) -> dict:
+def colour(input_namespace: str, input_block_name: str, color: str, universal_namespace: str = None, universal_block_name: str = None, carry_properties: Dict[str, List[str]] = None) -> dict:
 	if universal_namespace is None:
 		universal_namespace = input_namespace
 	if universal_block_name is None:
 		universal_block_name = input_block_name
-	return {
-		"to_universal": {
-			"new_block": f"{universal_namespace}:{universal_block_name}",
-			"new_properties": {
-				"color": colour
-			}
-		},
-		"from_universal": {
-			f"{universal_namespace}:{universal_block_name}": {
-				"map_properties": {
-					"color": {
-						colour: {
-							"new_block": f"{input_namespace}:{input_block_name}"
+	if carry_properties is None:
+		return {
+			"to_universal": {
+				"new_block": f"{universal_namespace}:{universal_block_name}",
+				"new_properties": {
+					"color": color
+				}
+			},
+			"from_universal": {
+				f"{universal_namespace}:{universal_block_name}": {
+					"map_properties": {
+						"color": {
+							color: {
+								"new_block": f"{input_namespace}:{input_block_name}"
+							}
 						}
 					}
 				}
 			}
 		}
-	}
+	else:
+		return {
+			"to_universal": {
+				"new_block": f"{universal_namespace}:{universal_block_name}",
+				"new_properties": {
+					"color": color
+				},
+				"carry_properties": carry_properties
+			},
+			"from_universal": {
+				f"{universal_namespace}:{universal_block_name}": {
+					"map_properties": {
+						"color": {
+							color: {
+								"new_block": f"{input_namespace}:{input_block_name}"
+							}
+						}
+					},
+					"carry_properties": carry_properties
+				}
+			}
+		}
 
 
 def anvil(input_namespace: str, input_block_name: str, damage: str, universal_namespace: str = None, universal_block_name: str = None) -> dict:
