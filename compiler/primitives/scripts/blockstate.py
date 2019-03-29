8b946f7692d4e1502b8a5da4bc045224fd6a1545
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-03-29 13:48:07 +0000

    [Primitives] Added a blockstate colour funciton

--- compiler/primitives/scripts/blockstate.py
+++ compiler/primitives/scripts/blockstate.py
@@ -26,3 +26,29 @@ def stone(input_namespace: str, input_block_name: str, polished: bool, universal
 			}
 		}
 	}
+
+
+def colour(input_namespace: str, input_block_name: str, colour: str, universal_namespace: str = None, universal_block_name: str = None) -> dict:
+	if universal_namespace is None:
+		universal_namespace = input_namespace
+	if universal_block_name is None:
+		universal_block_name = input_block_name
+	return {
+		"to_universal": {
+			"new_block": f"{universal_namespace}:{universal_block_name}",
+			"new_properties": {
+				"color": colour
+			}
+		},
+		"from_universal": {
+			f"{universal_namespace}:{universal_block_name}": {
+				"map_properties": {
+					"color": {
+						colour: {
+							"new_block": f"{input_namespace}:{input_block_name}"
+						}
+					}
+				}
+			}
+		}
+	}
