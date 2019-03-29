72899447f8229c005d50edfeae690f1a2e03a638
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-03-29 14:22:09 +0000

    [Primitives] Added anvil primitives

--- compiler/primitives/scripts/blockstate.py
+++ compiler/primitives/scripts/blockstate.py
@@ -52,3 +52,45 @@ def colour(input_namespace: str, input_block_name: str, colour: str, universal_n
 			}
 		}
 	}
+
+
+def anvil(input_namespace: str, input_block_name: str, damage: str, universal_namespace: str = None, universal_block_name: str = None) -> dict:
+	if universal_namespace is None:
+		universal_namespace = input_namespace
+	if universal_block_name is None:
+		universal_block_name = input_block_name
+	return {
+		"to_universal": {
+			"new_block": f"{universal_namespace}:{universal_block_name}",
+			"new_properties": {
+				"damage": damage
+			},
+			"carry_properties": {
+				"facing": [
+					"north",
+					"south",
+					"west",
+					"east"
+				]
+			}
+		},
+		"from_universal": {
+			f"{universal_namespace}:{universal_block_name}": {
+				"map_properties": {
+					"damage": {
+						damage: {
+							"new_block": f"{input_namespace}:{input_block_name}"
+						}
+					}
+				},
+				"carry_properties": {
+					"facing": [
+						"north",
+						"south",
+						"west",
+						"east"
+					]
+				}
+			}
+		}
+	}
