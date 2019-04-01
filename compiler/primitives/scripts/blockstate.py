19c59a82ed91d456bdcbbd7dcb9ad6689f3cc00a
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-04-01 16:45:45 +0100

    [Java 1.13.2] Added leaves

--- compiler/primitives/scripts/blockstate.py
+++ compiler/primitives/scripts/blockstate.py
@@ -375,3 +375,79 @@ def flower_pot(input_namespace: str, input_block_name: str, plant: str, universa
 			}
 		}
 	}
+
+
+def leaves(input_namespace: str, input_block_name: str, material: str, universal_namespace: str = None, universal_block_name: str = None) -> dict:
+	if universal_namespace is None:
+		universal_namespace = input_namespace
+	if universal_block_name is None:
+		universal_block_name = input_block_name
+	return {
+		"to_universal": {
+			"new_block": f"{universal_namespace}:{universal_block_name}",
+			"new_properties": {
+				"material": material
+			},
+			"carry_properties": {
+				"distance": [
+					"1",
+					"2",
+					"3",
+					"4",
+					"5",
+					"6",
+					"7"
+				]
+			},
+			"map_properties": {
+				"persistent": {
+					"true": {
+						"new_properties": {
+							"decayable": "false",
+							"check_decay": "false"
+						}
+					},
+					"false": {
+						"new_properties": {
+							"decayable": "true",
+							"check_decay": "false"
+						}
+					}
+				}
+			}
+		},
+		"from_universal": {
+			f"{universal_namespace}:{universal_block_name}": {
+				"map_properties": {
+					"material": {
+						material: {
+							"new_block": f"{input_namespace}:{input_block_name}"
+						}
+					},
+					"decayable": {
+						"true": {
+							"new_properties": {
+								"persistent": "false"
+							}
+						},
+						"false": {
+							"new_properties": {
+								"persistent": "true"
+							}
+						}
+					}
+				},
+				"carry_properties": {
+					"distance": [
+						"1",
+						"2",
+						"3",
+						"4",
+						"5",
+						"6",
+						"7"
+					]
+				}
+			}
+		}
+	}
