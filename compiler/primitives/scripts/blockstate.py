bbaf85ace7442c71804971a3f2abda57df584fa7
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-04-01 17:25:09 +0100

    [Java 1.13.2] Added wood and logs
    stripped and normal

--- compiler/primitives/scripts/blockstate.py
+++ compiler/primitives/scripts/blockstate.py
@@ -451,3 +451,51 @@ def leaves(input_namespace: str, input_block_name: str, material: str, universal
 			}
 		}
 	}
+
+
+def wood(input_namespace: str, input_block_name: str, material: str, stripped: bool, universal_namespace: str = None, universal_block_name: str = None) -> dict:
+	if universal_namespace is None:
+		universal_namespace = input_namespace
+	if universal_block_name is None:
+		universal_block_name = input_block_name
+	stripped = 'true' if stripped else 'false'
+	return {
+		"to_universal": {
+			"new_block": f"{universal_namespace}:{universal_block_name}",
+			"new_properties": {
+				"material": material,
+				"stripped": stripped
+			},
+			"carry_properties": {
+				"axis": [
+					"x",
+					"y",
+					"z"
+				]
+			}
+		},
+		"from_universal": {
+			f"{universal_namespace}:{universal_block_name}": {
+				"map_properties": {
+					"material": {
+						material: {
+							"map_properties": {
+								"stripped": {
+									stripped: {
+										"new_block": f"{input_namespace}:{input_block_name}"
+									}
+								}
+							}
+						}
+					}
+				},
+				"carry_properties": {
+					"axis": [
+						"x",
+						"y",
+						"z"
+					]
+				}
+			}
+		}
+	}
