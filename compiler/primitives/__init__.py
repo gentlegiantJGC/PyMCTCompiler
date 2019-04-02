e2e5a8dd5c3ce949df9f452d50a064877aaee381
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-04-02 15:00:19 +0100

    [Primitives] A list of primitives can now be given
    If a list of primitives is given it will deep merge the primitives into one dictionary. This is designed for entities and block entities to allow inheritance

--- compiler/primitives/__init__.py
+++ compiler/primitives/__init__.py
@@ -1,6 +1,7 @@
 import json
 import os
 import traceback
+from typing import Union, List
 from .scripts import *
 
 
@@ -42,7 +43,43 @@ for root, dirs, files in os.walk(f'{os.path.dirname(__file__)}/entities'):
 print('\tFinished Loading Primitives')
 
 
-def get_block(block_format: str, block_name: str) -> dict:
+def get_block(block_format: str, primitive: Union[str, List[str]]) -> dict:
 	assert block_format in blocks, f'"{block_format}" is not a known format'
-	assert block_name in blocks[block_format], f'"{block_name}" is not present in the mappings for format "{block_format}"'
-	return blocks[block_format][block_name]
+	if isinstance(primitive, str):
+		assert primitive in blocks[block_format], f'"{primitive}" is not present in the mappings for format "{block_format}"'
+		return blocks[block_format][primitive]
+	elif isinstance(primitive, list):
+		output = {}
+		for nested_primitive in primitive:
+			assert isinstance(nested_primitive, str), f'Expected a list of strings. At least one entry was type {type(nested_primitive)}'
+			assert nested_primitive in blocks[block_format], f'"{nested_primitive}" is not present in the mappings for format "{block_format}"'
+			output = merge_objects(output, blocks[block_format][nested_primitive])
+		return output
+	else:
+		raise Exception(f'Un-supported format: {type(primitive)}')
+
+
+def get_entity(primitive: Union[str, List[str]]) -> dict:
+	if isinstance(primitive, str):
+		assert primitive in entities, f'"{primitive}" is not present in the entity mappings'
+		return entities[primitive]
+	elif isinstance(primitive, list):
+		output = {}
+		for nested_primitive in primitive:
+			assert isinstance(nested_primitive, str), f'Expected a list of strings. At least one entry was type {type(nested_primitive)}'
+			assert nested_primitive in entities, f'"{nested_primitive}" is not present in the entity mappings'
+			output = merge_objects(output, entities[nested_primitive])
+		return output
+	else:
+		raise Exception(f'Un-supported format: {type(primitive)}')
+
+
+def merge_objects(obj1, obj2):
+	if isinstance(obj2, dict) and isinstance(obj1, dict):
+		for key, val in obj2.items():
+			if key in obj1:
+				obj1[key] = merge_objects(obj1[key], obj2[key])
+			else:
+				obj1[key] = val
+	else:
+		return obj2
