1ca04d88cbf20ba77f0fbd35a649d23a2cc98563
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-04-25 14:46:54 +0100

    [Reader] Added some disk read/write functions
    Once the data is read from disk it is kept in a format that is slightly easier to handle. At the end when saving it is converted to the disk format again.

---
+++ reader/helpers/version_code.py
@@ -0,0 +1,15 @@
+def blockentity_from_disk(version: Version, nbt: NBT) -> BlockEntity:
+	pass
+
+
+def entity_from_disk(version: Version, nbt: NBT) -> Entity:
+	pass
+
+
+
+def blockentity_to_disk(version: Version, nbt: BlockEntity) -> NBT:
+	pass
+
+
+def entity_to_disk(version: Version, nbt: Entity) -> NBT:
+	pass
