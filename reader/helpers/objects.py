5c8682c1d8cd75ec1c18e9628452e232ccc08af7
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-05-06 17:36:35 +0100

    Added more properties from the Amulet Block class

--- reader/helpers/objects.py
+++ reader/helpers/objects.py
@@ -1,11 +1,19 @@
-from typing import Dict, Union
+from typing import Dict, Union, Tuple
 import copy
+import re
 
 
 class Block:
 	"""
 	A minified version of the block class from the Amulet Editor.
 	"""
+
+	blockstate_regex = re.compile(
+		r"(?:(?P<namespace>[a-z0-9_.-]+):)?(?P<base_name>[a-z0-9/._-]+)(?:\[(?P<property_name>[a-z0-9_]+)=(?P<property_value>[a-z0-9_]+)(?P<properties>.*)\])?"
+	)
+
+	parameters_regex = re.compile(r"(?:,(?P<name>[a-z0-9_]+)=(?P<value>[a-z0-9_]+))")
+
 	def __init__(self, blockstate: str = None, namespace: str = None, base_name: str = None, properties: Dict[str, Union[str, bool, int]] = None):
 		self._blockstate = blockstate
 		self._namespace = namespace
@@ -62,6 +70,30 @@ class Block:
 
 	def __hash__(self) -> int:
 		return hash(self.blockstate)
+
+	@staticmethod
+	def parse_blockstate_string(blockstate: str) -> Tuple[str, str, Dict[str, str]]:
+		match = Block.blockstate_regex.match(blockstate)
+		namespace = match.group("namespace") or "minecraft"
+		base_name = match.group("base_name")
+
+		if match.group("property_name") is not None:
+			properties = {match.group("property_name"): match.group("property_value")}
+		else:
+			properties = {}
+
+		properties_string = match.group("properties")
+		if properties_string is not None:
+			properties_match = Block.parameters_regex.finditer(properties_string)
+			for match in properties_match:
+				properties[match.group("name")] = match.group("value")
+
+		return namespace, base_name, {k: v for k, v in sorted(properties.items())}
+
+	def uparse_blockstate_string(self):
+		self._namespace, self._base_name, self._properties = self.parse_blockstate_string(
+			self._blockstate
+		)
 
 
 class Entity:
