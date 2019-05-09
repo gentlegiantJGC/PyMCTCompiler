ebfad1242c0d8abc6dee0b2b57455d2772017b3f
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-05-09 18:13:45 +0100

    Cleanded up and full implemented the main convert function
    Note this is just a wrapper that sets up and handles the _convert function

--- reader/convert.py
+++ reader/convert.py
@@ -1,9 +1,9 @@
 from typing import Union, Tuple
 from .helpers.objects import Block, BlockEntity, Entity
+from .helpers.nbt import from_spec
 from .data_version_handler import SubVersion
 
 
-def convert(level, object_input: Union[Block, Entity], input_spec: dict, mappings: dict, output_version: SubVersion, location: Tuple[int, int, int] = None) -> Tuple[Union[Block, Entity], Union[BlockEntity, None], bool, bool]:
 def get_blockentity(level, location: Tuple[int, int, int]) -> BlockEntity:
 	"""Reach back into the level and pull the block entity from the given location
 	Should return a BlockEntity class or None if there is no BlockEntity at the given location
@@ -14,6 +14,7 @@ def get_blockentity(level, location: Tuple[int, int, int]) -> BlockEntity:
 		raise Exception('level is None and more data needed from it')
 
 
+def convert(level, object_input: Union[Block, Entity], input_spec: dict, mappings: dict, output_version: SubVersion, location: Tuple[int, int, int] = None, extra_input: BlockEntity = None) -> Tuple[Union[Block, Entity], Union[BlockEntity, None], bool, bool]:
 	"""
 		A demonstration function on how to read the json files to convert into or out of the numerical block_format
 		You should implement something along these lines into you own code if you want to read them.
@@ -24,6 +25,7 @@ def get_blockentity(level, location: Tuple[int, int, int]) -> BlockEntity:
 		:param mappings: the mapping file for the input_object
 		:param output_version: A way for the function to look at the specification being converted to. (used to load default properties)
 		:param location: (x, y, z) only used for Blocks if data beyond the object_input is needed
+		:param extra_input: secondary to the object_input a block entity can be given. This should only be used in the select block tool. Not compatible with location
 		:return: output, extra_output, extra_needed, cacheable
 			extra_needed: a bool to specify if more data is needed beyond the object_input
 			cacheable: a bool to specify if the result can be cached to reduce future processing
@@ -32,56 +34,29 @@ def get_blockentity(level, location: Tuple[int, int, int]) -> BlockEntity:
 			Entity, None, bool, bool
 	"""
 
-	extra_input = None
-
 	if isinstance(object_input, Block):
 		if 'nbt' in input_spec and location is not None:
-			# TODO: create blockentity from the nbt spec
-			extra_input = BlockEntity()
-	# TODO: overwrite with the data loaded from the world if present
+			# if the block location in the world is defined then load the BlockEntity from the world
+			extra_input = get_blockentity(level, location)
+			if extra_input is None:
+				# if there is no BlockEntity at location create it based off the specification
+				namespace, base_name = input_spec['nbt_identifier'].split(':', 1)
+				extra_input = BlockEntity(namespace, base_name, (0, 0, 0), from_spec(input_spec['nbt']))
+			# if the BlockEntity is already defined in extra_input continue with that
 
-	elif isinstance(object_input, Entity):
-		raise NotImplemented
-
-	# TODO: rework the NBT system
-	# if location is None:
-	# 	return object_input, None, True
-	# else:
-	# 	nbt = get_nbt(level, location)
-	# 	input_blockstate['nbt'] = {}
-	# 	for key in input_spec['nbt']:
-	# 		_nbt = nbt
-	# 		path = input_spec['nbt'][key].get('path', []) + [input_spec['nbt'][key]['name'], input_spec['nbt'][key]['type']]
-	# 		try:
-	# 			assert _nbt.__class__.__name__ == 'TAG_Compound'
-	# 			for path_key, dtype in path:
-	# 				_nbt = _nbt[path_key]
-	# 				assert _nbt.__class__.__name__ == {
-	# 					'compound': 'TAG_Compound',
-	# 					'list': 'TAG_List',
-	# 					'byte': 'TAG_Byte',
-	# 					'short': 'TAG_Short',
-	# 					'int': 'TAG_Int',
-	# 					'long': 'TAG_Long',
-	# 					'float': 'TAG_Float',
-	# 					'double': 'TAG_Double',
-	# 					'string': 'TAG_String'
-	# 				}[dtype]
-	# 			input_blockstate['nbt'][key] = str(_nbt.value)
-	# 		except:
-	# 			input_blockstate['nbt'][key] = input_spec['nbt'][key]['default']
-
-	if isinstance(object_input, Block):
+			# if location and extra_input are both None then continue with the mapping as normal but without the BlockEntity.
+			# The mappings will do as much as it can and will return the extra_needed flag as True telling the caller to find the location if possible
 		block_input = object_input
 		if extra_input is not None:
 			assert isinstance(extra_input, BlockEntity)
 			nbt_input = extra_input
 		else:
 			nbt_input = None
+
 	elif isinstance(object_input, Entity):
+		assert extra_input is None, 'When an Entity is the first input the extra input must be None'
 		block_input = None
 		nbt_input = object_input
-		raise NotImplemented
 	else:
 		raise Exception
 
