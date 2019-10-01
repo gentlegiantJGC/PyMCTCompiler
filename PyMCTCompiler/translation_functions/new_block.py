dabfe4932086d329454c5abbbc6a39aad5cccc00
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-10-01 15:48:03 +0100

    Implemented new block and new entity functions

--- PyMCTCompiler/translation_functions/new_block.py
+++ PyMCTCompiler/translation_functions/new_block.py
@@ -1,24 +1,30 @@
+from typing import Tuple
 from PyMCTCompiler.translation_functions import BaseTranslationFunction
 
 
 class NewBlock(BaseTranslationFunction):
 	function_name = 'new_block'
 
+	# {
+	# 	"function": "new_block",
+	# 	"options": "<namespace>:<base_name>"
+	# }
+
 	def __init__(self, data):
 		BaseTranslationFunction.__init__(self, data)
 
-	def primitive_add(self, other):
+	def _primitive_extend(self, other):
 		"""Used to merge two primitive files together.
 		The formats do not need to be identical but close enough that the data can stack."""
-		raise NotImplemented
+		self['options'] = other['options']
 
-	def compiled_add(self, other):
+	def _compiled_extend(self, other):
 		"""Used to merge two completed translations together.
 		The formats must match in such a way that the two base translations do not interfere."""
-		raise NotImplemented
+		assert self['options'] == other['options'], '"new_block" must be the same when merging'
 
-	def _validate_format(self):
-		raise NotImplemented
+	def _commit(self, extra_feature_set: Tuple[str, ...]):
+		assert isinstance(self['options'], str), '"options" must be a string'
 
 	def to_object(self):
-		raise NotImplemented
+		return self._function
