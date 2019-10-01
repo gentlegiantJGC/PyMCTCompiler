9d7c7888584639bba2a5813ed8cde6171ea858e3
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-10-01 12:50:28 +0100

    Added template classes for the new function setup

---
+++ PyMCTCompiler/translation_functions/map_properties.py
@@ -0,0 +1,24 @@
+from PyMCTCompiler.translation_functions import BaseTranslationFunction
+
+
+class MapProperties(BaseTranslationFunction):
+	function_name = 'map_properties'
+
+	def __init__(self, data):
+		BaseTranslationFunction.__init__(self, data)
+
+	def primitive_add(self, other):
+		"""Used to merge two primitive files together.
+		The formats do not need to be identical but close enough that the data can stack."""
+		raise NotImplemented
+
+	def compiled_add(self, other):
+		"""Used to merge two completed translations together.
+		The formats must match in such a way that the two base translations do not interfere."""
+		raise NotImplemented
+
+	def _validate_format(self):
+		raise NotImplemented
+
+	def to_object(self):
+		raise NotImplemented
