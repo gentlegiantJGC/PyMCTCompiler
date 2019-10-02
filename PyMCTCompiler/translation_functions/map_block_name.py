fed5457f73db6a0f2c22c348adfba1b1b1b83345
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-10-02 10:02:40 +0100

    Implemented map block name

--- PyMCTCompiler/translation_functions/map_block_name.py
+++ PyMCTCompiler/translation_functions/map_block_name.py
@@ -1,24 +1,53 @@
-from PyMCTCompiler.translation_functions import BaseTranslationFunction
+from typing import Tuple, Set
+import copy
+from PyMCTCompiler.translation_functions import BaseTranslationFunction, FunctionList
 
 
 class MapBlockName(BaseTranslationFunction):
 	function_name = 'map_block_name'
 
+	# {
+	# 	"function": "map_block_name",
+	# 	"options": {
+	# 		"<namespace>:<base_name>": [
+	# 			<functions>
+	# 		]
+	# 	}
+	# }
+
 	def __init__(self, data):
+		for option, data_ in data['options'].items():
+			data['options'][option] = FunctionList(data_)
 		BaseTranslationFunction.__init__(self, data)
 
-	def primitive_add(self, other):
+	def _primitive_extend(self, other: BaseTranslationFunction):
 		"""Used to merge two primitive files together.
 		The formats do not need to be identical but close enough that the data can stack."""
-		raise NotImplemented
+		for val in other['options']:
+			if val in self['options']:
+				self['options'][val].extend(other['options'][val])
+			else:
+				self['options'][val] = other['options'][val]
 
-	def compiled_add(self, other):
+	def _compiled_extend(self, other: BaseTranslationFunction):
 		"""Used to merge two completed translations together.
 		The formats must match in such a way that the two base translations do not interfere."""
-		raise NotImplemented
+		assert self['options'].keys() == other['options'].keys(), '"map_block_name" must have the same key entries when merging'
+		for key in other['options'].keys():
+			for val in other['options'][key].keys():
+				if val in self['options'][key].keys():
+					self['options'][key][val].extend(other['options'][key][val])
+				else:
+					self['options'][key][val] = other['options'][key][val]
 
-	def _validate_format(self):
-		raise NotImplemented
+	def _commit(self, feature_set: Set[str]):
+		assert isinstance(self['options'], dict), f'"options" must be a dictionary. Got {self["options"]} instead'
+		for key, val in self['options'].items():
+			assert isinstance(key, str), f'Key must be a string. Got {key}'
+			val.commit(feature_set)
 
-	def to_object(self):
-		raise NotImplemented
+	def to_object(self) -> dict:
+		data = copy.deepcopy(self._function)
+		for option, data_ in data['options'].items():
+			data['options'][option] = data_.to_object()
+		return data
