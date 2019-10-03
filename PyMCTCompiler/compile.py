6766be7d6e99d05f4c4a4a137a0b94376461c631
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-10-03 15:10:33 +0100

    Changed compiler to run in version order (oldest first)

--- PyMCTCompiler/compile.py
+++ PyMCTCompiler/compile.py
@@ -143,11 +143,23 @@ def main():
 	if isdir('', compiled_dir):
 		raise Exception(f'Failed to delete "{compiled_dir}" for some reason')
 	log_to_file('\tFinished deleting the mapping directory')
-	# iterate through all versions in the uncompiled directory
+
+	# sort versions into order by version number
+	versions = {}
 	for version_name in listdir(''):
 		if not isdir(f'./{version_name}') or version_name == '__pycache__':
 			continue
 		if hasattr(version_compiler, version_name) and hasattr(getattr(version_compiler, version_name), 'init'):
+			init = getattr(version_compiler, version_name).init
+			assert isinstance(init, dict)
+			versions.setdefault(init["platform"], {})[tuple(init["version"])] = version_name
+		else:
+			log_to_file(f'Could not find __init__.json file for {version_name} This version has been skipped')
+
+	# iterate through all versions in the uncompiled directory
+	for platform in sorted(versions.keys()):
+		for version in sorted(versions[platform]):
+			version_name = versions[platform][version]
 			log_to_file(f'Compiling {version_name} ...')
 			t = time.time()
 			init = getattr(version_compiler, version_name).init
@@ -166,8 +178,7 @@ def main():
 				log_to_file(f'\tFinished in {round(time.time() - t, 2)} seconds')
 			else:
 				log_to_file(f'"block_format" in __init__.json for {version_name} is either not defined or not a valid value. This version has been skipped')
-		else:
-			log_to_file(f'Could not find __init__.json file for {version_name} This version has been skipped')
+
 	log_to_file(f'\nFinished compiling all versions in {round(time.time() - t2, 2)}')
 
 
