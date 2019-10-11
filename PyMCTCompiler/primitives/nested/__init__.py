e72b33c3d8e135758bb7943e1eca35f464b2b60a
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-10-11 15:18:18 +0100

    Finished up nested primitives module

--- PyMCTCompiler/primitives/nested/__init__.py
+++ PyMCTCompiler/primitives/nested/__init__.py
@@ -1,7 +1,7 @@
 import os
 import traceback
 import copy
-from typing import Union, List, Dict
+from typing import Union, List, Dict, Set
 from PyMCTCompiler.translation_functions import FunctionList
 from PyMCTCompiler.primitives import _load_file
 
@@ -22,22 +22,29 @@ for root, _, files in os.walk(f'{os.path.dirname(__file__)}/data'):
 print('\tFinished Loading Nested Primitives')
 
 
-def get(primitive_group: Union[str, List[str]]) -> FunctionList:
+def _recursive_to_tuple(data):
+	if isinstance(data, list):
+		return tuple(_recursive_to_tuple(d) for d in data)
+	else:
+		return data
+
+
+def get(primitive_group: Union[str, List[str], List[List[str]]], parents: list, feature_set: Set[str] = None) -> FunctionList:
+	primitive_group = _recursive_to_tuple(primitive_group)
+
 	if primitive_group in nested_primitives:
 		return copy.deepcopy(nested_primitives[primitive_group])
 	else:
 		if isinstance(primitive_group, str):
 			assert primitive_group in nested_primitives, f'"{primitive_group}" is not present in nested primitives'
 			output = copy.deepcopy(nested_primitives[primitive_group])
-			output.commit(None, [])
 			return output
-		elif isinstance(primitive_group, list):
-			output = FunctionList([])
-			for primitive in primitive_group:
-				assert isinstance(primitive, str), f'Expected a list of strings. At least one entry was type {type(primitive)}'
-				assert primitive in nested_primitives, f'"{primitive}" is not present in the entity mappings'
-				output.extend(copy.deepcopy(nested_primitives[primitive]), [])
-			output.commit(None, [])
+		elif isinstance(primitive_group, tuple):
+			assert len(primitive_group) > 0, 'There was no data in the requested primitive group'
+			output = get(primitive_group[0], parents)
+			for primitive in primitive_group[1:]:
+				output.extend(get(primitive, parents), parents)
+			output.commit(feature_set, parents)
 			return output
 		else:
-			raise Exception(f'Un-supported format: {type(primitive_group)}')
+			raise Exception(f'Un-supported format: {primitive_group}')
