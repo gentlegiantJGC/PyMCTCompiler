46da44c9bdc6f3f4fee5a1b0a8d3b4371c2a8871
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-11-18 19:41:44 +0000

    Hooked up B1.13 to the nbt blockstate compiler

--- PyMCTCompiler/version_compiler/bedrock_1_13_0/__init__.py
+++ PyMCTCompiler/version_compiler/bedrock_1_13_0/__init__.py
@@ -1,10 +1,10 @@
-from PyMCTCompiler.compilers.numerical_compiler import NumericalCompiler
+from PyMCTCompiler.compilers.nbt_blockstate_compiler import NBTBlockstateCompiler
 import os
 
-compiler = NumericalCompiler(
+compiler = NBTBlockstateCompiler(
     os.path.dirname(__file__),
     block_format="nbt-blockstate",
     entity_format="namespace-str-id",
     version=[1, 13, 0],
-    parent_version='bedrock_1_7_0'
+    parent_version='bedrock_1_11_4'
 )
