120469e837ce11ea0be85988dfe27103eaefad89
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-11-26 15:18:12 +0000

    Added more common classes

--- PyMCTCompiler/primitives/scripts/nbt/common.py
+++ PyMCTCompiler/primitives/scripts/nbt/common.py
@@ -1,9 +1,48 @@
 from PyMCTCompiler.primitives.scripts.nbt import NBTRemapHelper
 
+java_custom_name = NBTRemapHelper(
+    [(
+        ("CustomName", "string", []),
+        ("CustomName", "string", [("utags", "compound")])
+    )],
+)
+
 java_str_lock = NBTRemapHelper(
     [(
         ("Lock", "string", []),
         ("Lock", "string", [("utags", "compound")])
     )],
     "{Lock: \"\"}"
+)
+
+java_items_3 = NBTRemapHelper(
+    [(
+        ("Items", "list", []),
+        ("Items", "list", [("utags", "compound")])
+    )],
+    "{Items: []}"
+)
+
+java_items_5 = NBTRemapHelper(
+    [(
+        ("Items", "list", []),
+        ("Items", "list", [("utags", "compound")])
+    )],
+    "{Items: []}"
+)
+
+java_items_9 = NBTRemapHelper(
+    [(
+        ("Items", "list", []),
+        ("Items", "list", [("utags", "compound")])
+    )],
+    "{Items: []}"
+)
+
+java_items_27 = NBTRemapHelper(
+    [(
+        ("Items", "list", []),
+        ("Items", "list", [("utags", "compound")])
+    )],
+    "{Items: []}"
 )
