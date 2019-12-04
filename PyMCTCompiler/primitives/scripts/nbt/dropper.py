95821c64e127dc760b639d5d6a14add6caf7c89c
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2019-12-04 19:12:39 +0000

    Added dropper

--- PyMCTCompiler/primitives/scripts/nbt/dropper.py
+++ PyMCTCompiler/primitives/scripts/nbt/dropper.py
@@ -8,9 +8,21 @@ J113    "minecraft:dropper"		"{Items: [], Lock: \"\"}"
 B113	"Dropper"		"{Items: [], isMovable: 1b}"
 """
 
+j112 = merge(
+    [EmptyNBT('minecraft:dropper'), java_custom_name, java_str_lock, java_items_9, java_loot_table],
+    ['universal_minecraft:dropper'],
+    abstract=True
+)
+
 j113 = merge(
     [EmptyNBT('minecraft:dropper'), java_custom_name, java_str_lock, java_items_9, java_loot_table],
     ['universal_minecraft:dropper']
+)
+
+b17 = merge(
+    [EmptyNBT('minecraft:dropper'), bedrock_items_9, bedrock_is_movable],
+    ['universal_minecraft:dropper'],
+    abstract=True
 )
 
 b113 = merge(
