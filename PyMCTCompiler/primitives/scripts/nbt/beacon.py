744ab7aef223019c81969414e8bf46a9604e84cb
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2019-12-04 15:40:10 +0000

    Added beacon

--- PyMCTCompiler/primitives/scripts/nbt/beacon.py
+++ PyMCTCompiler/primitives/scripts/nbt/beacon.py
@@ -3,12 +3,14 @@ from .common import java_str_lock, bedrock_is_movable
 
 """
 Default
-J113    "minecraft:beacon"		"{Secondary: 0, Primary: 0, Levels: -1, Lock: \"\"}"
+J112    "minecraft:beacon"      {Secondary: 0, Primary: 0, Levels: 0, Lock: ""}
+J113    "minecraft:beacon"		{Secondary: 0, Primary: 0, Levels: -1, Lock: ""}
 
-B113	"Beacon"                "{isMovable: 1b, primary: 0, secondary: 0}",
+
+B113	"Beacon"                {isMovable: 1b, primary: 0, secondary: 0}
 """
 
-_J113 = NBTRemapHelper(
+_J112 = NBTRemapHelper(
     [
         (
             ("Primary", "int", []),
@@ -40,9 +42,21 @@ _B113 = NBTRemapHelper(
     "{primary: 0, secondary: 0}"
 )
 
+j112 = merge(
+    [EmptyNBT('minecraft:beacon'), _J112, java_str_lock],
+    ['universal_minecraft:beacon'],
+    abstract=True
+)
+
 j113 = merge(
-    [EmptyNBT('minecraft:beacon'), _J113, java_str_lock],
+    [EmptyNBT('minecraft:beacon'), _J112, java_str_lock],
     ['universal_minecraft:beacon']
+)
+
+b17 = merge(
+    [EmptyNBT('minecraft:beacon'), _B113, bedrock_is_movable],
+    ['universal_minecraft:beacon'],
+    abstract=True
 )
 
 b113 = merge(
