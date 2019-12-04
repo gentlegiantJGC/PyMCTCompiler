cb52476cfb867e7cb6255f1297c9ca9f07ecb3d1
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2019-12-04 16:35:43 +0000

    added J1.12 comparator

--- PyMCTCompiler/primitives/scripts/nbt/comparator.py
+++ PyMCTCompiler/primitives/scripts/nbt/comparator.py
@@ -3,12 +3,13 @@ from .common import bedrock_is_movable
 
 """
 Default
-J113    "minecraft:comparator"		"{OutputSignal: 0}"
+J112                                {OutputSignal: 0}
+J113    "minecraft:comparator"		{OutputSignal: 0}
 
-B113	"Comparator"		"{OutputSignal: 0, isMovable: 1b}"
+B113	"Comparator"		        {OutputSignal: 0, isMovable: 1b}
 """
 
-_J113 = NBTRemapHelper(
+_J112 = NBTRemapHelper(
     [
         (
             ("OutputSignal", "int", []),
@@ -18,12 +19,24 @@ _J113 = NBTRemapHelper(
     "{OutputSignal: 0}"
 )
 
+j112 = merge(
+    [EmptyNBT('minecraft:comparator'), _J112],
+    ['universal_minecraft:comparator'],
+    abstract=True
+)
+
 j113 = merge(
-    [EmptyNBT('minecraft:comparator'), _J113],
+    [EmptyNBT('minecraft:comparator'), _J112],
     ['universal_minecraft:comparator']
 )
 
+b17 = merge(
+    [EmptyNBT('minecraft:comparator'), _J112, bedrock_is_movable],
+    ['universal_minecraft:comparator'],
+    abstract=True
+)
+
 b113 = merge(
-    [EmptyNBT('minecraft:comparator'), _J113, bedrock_is_movable],
+    [EmptyNBT('minecraft:comparator'), _J112, bedrock_is_movable],
     ['universal_minecraft:comparator']
 )
