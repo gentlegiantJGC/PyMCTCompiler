a7b981ea0de350176ffbadc7a7ac676a6bcf284a
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2019-12-04 19:04:14 +0000

    added daylight detector

--- PyMCTCompiler/primitives/scripts/nbt/daylight_detector.py
+++ PyMCTCompiler/primitives/scripts/nbt/daylight_detector.py
@@ -8,9 +8,21 @@ J113    "minecraft:daylight_detector"		"{}"
 B113	"DaylightDetector"		"{isMovable: 1b}"
 """
 
+j112 = merge(
+    [EmptyNBT('minecraft:daylight_detector')],
+    ['universal_minecraft:daylight_detector'],
+    abstract=True
+)
+
 j113 = merge(
     [EmptyNBT('minecraft:daylight_detector')],
     ['universal_minecraft:daylight_detector']
+)
+
+b17 = merge(
+    [EmptyNBT('minecraft:daylight_detector'), bedrock_is_movable],
+    ['universal_minecraft:daylight_detector'],
+    abstract=True
 )
 
 b113 = merge(
