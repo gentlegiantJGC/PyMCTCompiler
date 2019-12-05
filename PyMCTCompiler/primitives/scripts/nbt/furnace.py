e947865c97f0f72ebfe4c4c787f39f3be2404f27
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2019-12-05 14:25:21 +0000

    added furnace

--- PyMCTCompiler/primitives/scripts/nbt/furnace.py
+++ PyMCTCompiler/primitives/scripts/nbt/furnace.py
@@ -8,11 +8,12 @@ J113    "minecraft:furnace"		"{CookTime: 0s, BurnTime: 0s, Items: [], RecipesUse
 
 B113	"Furnace"		"{BurnDuration: 0s, BurnTime: 0s, CookTime: 0s, Items: [], StoredXPInt: 0, isMovable: 1b}"
 
-Full
-J113    "minecraft:furnace"		"{Lock: \"\", BurnTime: 0s, CookTime: 0s, CookTimeTotal: 0s, Items: [], RecipesUsedSize: 0s, RecipeLocationX: "", RecipeAmountX: 0s, CustomName: ""}"
+Full    "minecraft:furnace"
+J112    "{BurnTime: 0s, CookTime: 0s, CookTimeTotal: 0s, CustomName: \"\", Items: [], Lock: \"\"}"
+J113    "{BurnTime: 0s, CookTime: 0s, CookTimeTotal: 0s, CustomName: \"\", Items: [], Lock: \"\", RecipesUsedSize: 0s, RecipeLocationX: "", RecipeAmountX: 0s}"
 """
 
-_J113 = NBTRemapHelper(
+_J19 = NBTRemapHelper(
     [
         (
             ("BurnTime", "short", []),
@@ -26,13 +27,19 @@ _J113 = NBTRemapHelper(
             ("CookTimeTotal", "short", []),
             ("CookTimeTotal", "short", [("utags", "compound")])
         ),
+    ],
+    "{CookTime: 0s, BurnTime: 0s, CookTimeTotal: 0s}"
+)
+
+# TODO: sort out the variable keys
+_J113 = NBTRemapHelper(
+    [
         (
             ("RecipesUsedSize", "short", []),
             ("RecipesUsedSize", "short", [("utags", "compound")])
-        ),
-
+        )
     ],
-    "{CookTime: 0s, BurnTime: 0s, RecipesUsedSize: 0s, CookTimeTotal: 0s}"
+    "{RecipesUsedSize: 0s}"
 )
 
 _B113 = NBTRemapHelper(
@@ -58,9 +65,21 @@ _B113 = NBTRemapHelper(
     "{BurnDuration: 0s, BurnTime: 0s, CookTime: 0s, StoredXPInt: 0}"
 )
 
+j19 = merge(
+    [EmptyNBT('minecraft:furnace'), _J19, java_items_3, java_custom_name, java_str_lock],
+    ['universal_minecraft:furnace'],
+    abstract=True
+)
+
 j113 = merge(
-    [EmptyNBT('minecraft:furnace'), _J113, java_items_3, java_custom_name, java_str_lock],
+    [EmptyNBT('minecraft:furnace'), _J19, _J113, java_items_3, java_custom_name, java_str_lock],
     ['universal_minecraft:furnace']
+)
+
+b17 = merge(
+    [EmptyNBT('minecraft:furnace'), _B113, bedrock_items_3, bedrock_is_movable],
+    ['universal_minecraft:furnace'],
+    abstract=True
 )
 
 b113 = merge(
