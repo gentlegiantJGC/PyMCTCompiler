dc8ce9e5ec09898b9bfb2946dc2da782c3039867
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2019-12-05 10:04:34 +0000

    added end portals and end gateways

--- PyMCTCompiler/primitives/scripts/nbt/end_portal.py
+++ PyMCTCompiler/primitives/scripts/nbt/end_portal.py
@@ -8,9 +8,21 @@ J113    "minecraft:end_portal"		"{}"
 B113	"EndPortal"		"{isMovable: 1b}"
 """
 
+j112 = merge(
+    [EmptyNBT('minecraft:end_portal')],
+    ['universal_minecraft:end_portal'],
+    abstract=True
+)
+
 j113 = merge(
     [EmptyNBT('minecraft:end_portal')],
     ['universal_minecraft:end_portal']
+)
+
+b17 = merge(
+    [EmptyNBT('minecraft:end_portal'), bedrock_is_movable],
+    ['universal_minecraft:end_portal'],
+    abstract=True
 )
 
 b113 = merge(
