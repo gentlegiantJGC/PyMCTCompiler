f92a87e57470fa757b4006453e16d2d7ef0b6f3d
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-01-10 11:31:47 +0000

    Added placeholders for the new versions
    This will allow the basic versions to be created

--- PyMCTCompiler/version_compiler/java_1_13_2/__init__.py
+++ PyMCTCompiler/version_compiler/java_1_13_2/__init__.py
@@ -3,7 +3,6 @@ import os
 
 compiler = JavaBlockstateCompiler(
     os.path.dirname(__file__),
-    block_format="blockstate",
     version=[1, 13, 2],
     parent_version='java_1_13_1',
     data_version=1631
--- PyMCTCompiler/version_compiler/java_1_14_0/__init__.py
+++ PyMCTCompiler/version_compiler/java_1_14_0/__init__.py
@@ -3,7 +3,7 @@ import os
 
 compiler = JavaBlockstateCompiler(
     os.path.dirname(__file__),
-    version=[1, 13, 1],
-    parent_version='java_1_13_0',
-    data_version=1628
+    version=[1, 14, 0],
+    parent_version='java_1_13_2',
+    data_version=1952
 )
