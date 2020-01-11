ca77a73e886224f173175a10622b0b67153b384f
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-01-11 16:21:35 +0000

    Added the template for B1.14

---
+++ PyMCTCompiler/version_compiler/bedrock_1_14_0/__init__.py
@@ -0,0 +1,8 @@
+from PyMCTCompiler.compilers.nbt_blockstate_compiler import NBTBlockstateCompiler
+import os
+
+compiler = NBTBlockstateCompiler(
+    os.path.dirname(__file__),
+    version=[1, 14, 0],
+    parent_version='bedrock_1_13_0'
+)
