53e79539336f20467e35f9959721631c1e3a79ea
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-01-14 15:51:47 +0000

    Added bell and barrel to Bedrock 1.12-

--- PyMCTCompiler/primitives/scripts/nbt/bell.py
+++ PyMCTCompiler/primitives/scripts/nbt/bell.py
@@ -1,8 +1,13 @@
 universal = {
-    "nbt_identifier": ["universal_minecraft", "conduit"],
+    "nbt_identifier": ["universal_minecraft", "bell"],
     "snbt": """{
         utags: {
             isMovable: 1b
         }
     }"""
 }
+
+"""
+Bedrock 1.14
+{Direction: 0b, Ringing: 0b, Ticks: 0b, isMovable: 1b}  # check these types
+"""
