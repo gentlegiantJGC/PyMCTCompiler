55cfa623e2b4db2c14916b5f77da92acba824b1d
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-01-17 11:58:52 +0000

    Make the translation files public

--- PyMCTCompiler/disk_buffer.py
+++ PyMCTCompiler/disk_buffer.py
@@ -11,8 +11,7 @@ if TYPE_CHECKING:
 
 class DiskBuffer:
 	def __init__(self):
-		self._specification: Dict[Tuple[str, str, str, str, str, str], dict] = {}
-		self._translations: Dict[str, Dict[Tuple[str, str, str, str, str, str], 'FunctionList']] = {
+		self.translations: Dict[str, Dict[Tuple[str, str, str, str, str, str], 'FunctionList']] = {
 			"to_universal": {},
 			"from_universal": {}
 		}
@@ -48,10 +47,10 @@ class DiskBuffer:
 
 	def add_translation_to_universal(self, version_name: str, object_type: str, version_format: str, namespace: str, group_name: str, base_name: str, data: 'FunctionList'):
 		"""add a translation file from version to universal format to the disk buffer to be saved at the end"""
-		self._translations["to_universal"][(version_name, object_type, version_format, namespace, group_name, base_name)] = data
+		self.translations["to_universal"][(version_name, object_type, version_format, namespace, group_name, base_name)] = data
 		
 	def has_translation_to_universal(self, version_name: str, object_type: str, version_format: str, namespace: str, group_name: str, base_name: str) -> bool:
-		return (version_name, object_type, version_format, namespace, group_name, base_name) in self._translations["to_universal"]
+		return (version_name, object_type, version_format, namespace, group_name, base_name) in self.translations["to_universal"]
 
 	def add_translation_from_universal(self, version_name: str, object_type: str, version_format: str, namespace: str, group_name: str, base_name: str, data: 'FunctionList'):
 		"""add a translation file from universal to version format to the disk buffer to be saved at the end.
@@ -65,10 +64,10 @@ class DiskBuffer:
 		:param data:
 		:return:
 		"""
-		if (version_name, object_type, version_format, namespace, group_name, base_name) in self._translations["from_universal"]:
-			self._translations["from_universal"][(version_name, object_type, version_format, namespace, group_name, base_name)].extend(data, [])
+		if (version_name, object_type, version_format, namespace, group_name, base_name) in self.translations["from_universal"]:
+			self.translations["from_universal"][(version_name, object_type, version_format, namespace, group_name, base_name)].extend(data, [])
 		else:
-			self._translations["from_universal"][(version_name, object_type, version_format, namespace, group_name, base_name)] = data
+			self.translations["from_universal"][(version_name, object_type, version_format, namespace, group_name, base_name)] = data
 
 	def save_nested_translation(self, primitive_group: Union[Tuple[str, ...], Tuple[Tuple[str, ...], ...]], data: list):
 		"""This method should only be used by internal code.
@@ -97,8 +96,8 @@ class DiskBuffer:
 
 	def save(self):
 		log_to_file('Saving to disk')
-		for direction in self._translations:
-			for path, data in self._translations[direction].items():
+		for direction in self.translations:
+			for path, data in self.translations[direction].items():
 				data.commit(None, [])  # validate the translation
 				self.save_json_object(('versions',) + path[:3] + (direction,) + path[3:], data.save([]))  # add the file to the dictionary to be saved
 
