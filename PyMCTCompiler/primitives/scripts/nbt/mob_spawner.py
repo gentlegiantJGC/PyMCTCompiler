3afa61b2aea4d9755949216fbd8ff2aad3db7dae
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-01-21 14:40:12 +0000

    Added spawner translations

--- PyMCTCompiler/primitives/scripts/nbt/mob_spawner.py
+++ PyMCTCompiler/primitives/scripts/nbt/mob_spawner.py
@@ -5,19 +5,33 @@ from .common import bedrock_is_movable
 Default
 J113    "minecraft:mob_spawner"		"{MaxNearbyEntities: 6s, RequiredPlayerRange: 16s, SpawnCount: 4s, SpawnData: {id: \"minecraft:pig\"}, MaxSpawnDelay: 800s, Delay: 20s, SpawnRange: 4s, MinSpawnDelay: 200s, SpawnPotentials: [{Entity: {id: \"minecraft:pig\"}, Weight: 1}]}"
 
-B113	"MobSpawner"		"{Delay: 20s, DisplayEntityHeight: 1.7999999523162842f, DisplayEntityScale: 1.0f, DisplayEntityWidth: 0.800000011920929f, EntityIdentifier: \"\", MaxNearbyEntities: 6s, MaxSpawnDelay: 800s, MinSpawnDelay: 200s, RequiredPlayerRange: 16s, SpawnCount: 4s, SpawnRange: 4s, isMovable: 1b}"
+B113	"MobSpawner"		        "{MaxNearbyEntities: 6s, RequiredPlayerRange: 16s, SpawnCount: 4s, EntityIdentifier: \"\",             MaxSpawnDelay: 800s, Delay: 20s, SpawnRange: 4s, MinSpawnDelay: 200s, DisplayEntityHeight: 1.8f, DisplayEntityScale: 1.0f, DisplayEntityWidth: 0.8f, isMovable: 1b}"
 """
 
 universal = {
     "nbt_identifier": ["universal_minecraft", "spawner"],
     "snbt": """{
         utags: {
-            isMovable: 1b
+            isMovable: 1b,
+            MaxNearbyEntities: 6s,
+            RequiredPlayerRange: 16s,
+            MaxNearbyEntities: 6s, 
+            RequiredPlayerRange: 16s, 
+            SpawnCount: 4s, 
+            SpawnData: {id: "minecraft:pig"}, 
+            EntityIdentifier: "", 
+            MaxSpawnDelay: 800s, 
+            Delay: 20s, 
+            SpawnRange: 4s, 
+            MinSpawnDelay: 200s,
+            DisplayEntityHeight: 1.8f, 
+            DisplayEntityScale: 1.0f, 
+            DisplayEntityWidth: 0.8f
         }
     }"""
 }
 
-_J113 = NBTRemapHelper(
+_JBShared = NBTRemapHelper(
     [
         (
             ("MaxNearbyEntities", "short", []),
@@ -30,10 +44,6 @@ _J113 = NBTRemapHelper(
         (
             ("SpawnCount", "short", []),
             ("SpawnCount", "short", [("utags", "compound")])
-        ),
-        (
-            ("SpawnData", "compound", []),
-            ("SpawnData", "compound", [("utags", "compound")])
         ),
         (
             ("MaxSpawnDelay", "short", []),
@@ -50,22 +60,69 @@ _J113 = NBTRemapHelper(
         (
             ("MinSpawnDelay", "short", []),
             ("MinSpawnDelay", "short", [("utags", "compound")])
+        )
+    ],
+    '{MaxNearbyEntities: 6s, RequiredPlayerRange: 16s, SpawnCount: 4s, MaxSpawnDelay: 800s, Delay: 20s, SpawnRange: 4s, MinSpawnDelay: 200s}'
+)
+
+_J19 = NBTRemapHelper(
+    [
+        (
+            ("SpawnData", "compound", []),
+            ("SpawnData", "compound", [("utags", "compound")])
+        ),
+        (
+            ("id", "string", [("SpawnData", "compound")]),
+            ("EntityIdentifier", "string", [("utags", "compound")])
         ),
         (
             ("SpawnPotentials", "compound", []),
             ("SpawnPotentials", "compound", [("utags", "compound")])
         )
     ],
-    '{MaxNearbyEntities: 6s, RequiredPlayerRange: 16s, SpawnCount: 4s, SpawnData: {id: "minecraft:pig"}, MaxSpawnDelay: 800s, Delay: 20s, SpawnRange: 4s, MinSpawnDelay: 200s, SpawnPotentials: [{Entity: {id: "minecraft:pig"}, Weight: 1}]}'
+    '{SpawnData: {id: "minecraft:pig"}, SpawnPotentials: [{Entity: {id: "minecraft:pig"}, Weight: 1}]}'
+)
+
+_B17 = NBTRemapHelper(
+    [
+        (
+            ("EntityIdentifier", "string", []),
+            ("EntityIdentifier", "string", [("utags", "compound")])
+        ),
+        (
+            ("DisplayEntityHeight", "float", []),
+            ("DisplayEntityHeight", "float", [("utags", "compound")])
+        ),
+        (
+            ("DisplayEntityScale", "float", []),
+            ("DisplayEntityScale", "float", [("utags", "compound")])
+        ),
+        (
+            ("DisplayEntityWidth", "float", []),
+            ("DisplayEntityWidth", "float", [("utags", "compound")])
+        )
+    ],
+    '{EntityIdentifier: "", DisplayEntityHeight: 1.8f, DisplayEntityScale: 1.0f, DisplayEntityWidth: 0.8f}'
+)
+
+j19 = merge(
+    [EmptyNBT('minecraft:mob_spawner'), _JBShared, _J19],
+    ['universal_minecraft:spawner'],
+    abstract=True
 )
 
 j113 = merge(
-    [EmptyNBT('minecraft:mob_spawner'), _J113],
+    [EmptyNBT('minecraft:mob_spawner'), _JBShared, _J19],
     ['universal_minecraft:spawner']
 )
 
-# TODO:
+b17 = merge(
+    [EmptyNBT('minecraft:mob_spawner'), _JBShared, _B17, bedrock_is_movable],
+    ['universal_minecraft:spawner'],
+    abstract=True
+)
+
 b113 = merge(
-    [EmptyNBT('minecraft:mob_spawner'), bedrock_is_movable],
+    [EmptyNBT('minecraft:mob_spawner'), _JBShared, _B17, bedrock_is_movable],
     ['universal_minecraft:spawner']
 )
