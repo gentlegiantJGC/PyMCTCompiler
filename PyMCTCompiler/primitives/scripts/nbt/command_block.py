ff4652b267a249dd876614f712f313176351d0fc
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-01-23 13:52:21 +0000

    Cleaned up command block block entity and added universal keys

--- PyMCTCompiler/primitives/scripts/nbt/command_block.py
+++ PyMCTCompiler/primitives/scripts/nbt/command_block.py
@@ -3,7 +3,7 @@ from .common import bedrock_is_movable, java_keep_packed
 
 """
 Default
-J113    "minecraft:command_block"		'{conditionMet: 0b, auto: 0b, CustomName: "{\\"text\\":\\"@\\"}", powered: 0b, Command: "", SuccessCount: 0, TrackOutput: 1b, UpdateLastExecution: 1b}'
+J113    "minecraft:command_block"		'{conditionMet: 0b, auto: 0b, CustomName: "{\"text\":\"@\"}", powered: 0b, Command: "", SuccessCount: 0, TrackOutput: 1b, UpdateLastExecution: 1b}'
 
 B113	"CommandBlock"		            "{Command: "", CustomName: "", ExecuteOnFirstTick: 0b, LPCommandMode: 16064, LPCondionalMode: 63b, LPRedstoneMode: 0b, LastExecution: 0L, LastOutput: "", LastOutputParams: [], SuccessCount: 0, TickDelay: 0, TrackOutput: 1b, Version: 10, auto: 0b, conditionMet: 0b, isMovable: 1b, powered: 0b}"
 """
@@ -12,7 +12,25 @@ universal = {
     "nbt_identifier": ["universal_minecraft", "command_block"],
     "snbt": """{
         utags: {
-            isMovable: 1b
+            isMovable: 1b,
+            auto: 0b,
+            Command: "",
+            conditionMet: 0b,
+            CustomName: "{\\"text\\":\\"@\\"}",
+            ExecuteOnFirstTick: 0b,
+            LPCommandMode: 0,
+            LPCondionalMode: 0b,
+            LPRedstoneMode: 0b,
+            LastExecution: 0l,
+            LastOutput: "",
+            LastOutputParams: [],
+            powered: 0b,
+            SuccessCount: 0,
+            TickDelay: 0,
+            TrackOutput: 1b,
+            Version: 10,
+            
+            UpdateLastExecution: 1b
         }
     }"""
 }
@@ -20,7 +38,7 @@ universal = {
 _J19 = NBTRemapHelper(
     [
         (
-            ("auto", "byte", []),
+            ("auto", "byte", []),  # does not need power
             ("auto", "byte", [("utags", "compound")])
         ),
         (
@@ -31,9 +49,17 @@ _J19 = NBTRemapHelper(
             ("conditionMet", "byte", []),
             ("conditionMet", "byte", [("utags", "compound")])
         ),
-        (  # this is JSON TODO: write a code script for it
+        (
             ("CustomName", "string", []),
             ("CustomName", "string", [("utags", "compound")])
+        ),
+        (
+            ("LastOutput", "string", []),
+            ("LastOutput", "string", [("utags", "compound")])
+        ),
+        (
+            ("LastExecution", "long", []),
+            ("LastExecution", "long", [("utags", "compound")])
         ),
         (
             ("powered", "byte", []),
@@ -46,14 +72,6 @@ _J19 = NBTRemapHelper(
         (
             ("TrackOutput", "byte", []),
             ("TrackOutput", "byte", [("utags", "compound")])
-        ),
-        (
-            ("LastOutput", "string", []),
-            ("LastOutput", "string", [("utags", "compound")])
-        ),
-        (
-            ("LastExecution", "long", []),
-            ("LastExecution", "long", [("utags", "compound")])
         )
     ],
     '{conditionMet: 0b, auto: 0b, CustomName: "{\\"text\\":\\"@\\"}", powered: 0b, Command: "", SuccessCount: 0, TrackOutput: 1b}'
@@ -82,59 +100,27 @@ _J112_update_last = NBTRemapHelper(
 _B113 = NBTRemapHelper(
     [
         (
-            ("conditionMet", "byte", []),
-            ("conditionMet", "byte", [("utags", "compound")])
-        ),
-        (
-            ("auto", "byte", []),
+            ("auto", "byte", []),  # does not need power
             ("auto", "byte", [("utags", "compound")])
-        ),
-        (
-            ("powered", "byte", []),
-            ("powered", "byte", [("utags", "compound")])
         ),
         (
             ("Command", "string", []),
             ("Command", "string", [("utags", "compound")])
         ),
         (
-            ("SuccessCount", "int", []),
-            ("SuccessCount", "int", [("utags", "compound")])
+            ("conditionMet", "byte", []),
+            ("conditionMet", "byte", [("utags", "compound")])
         ),
-        (
-            ("TrackOutput", "byte", []),
-            ("TrackOutput", "byte", [("utags", "compound")])
-        ),
-        (
+        (  # TODO: convert this to the json rawtext format
             ("CustomName", "string", []),
             ("CustomName", "string", [("utags", "compound")])
-        ),
-        (
-            ("LastOutput", "string", []),
-            ("LastOutput", "string", [("utags", "compound")])
-        ),
-        (
-            ("LastExecution", "long", []),
-            ("LastExecution", "long", [("utags", "compound")])
         ),
         (
             ("ExecuteOnFirstTick", "byte", []),
             ("ExecuteOnFirstTick", "byte", [("utags", "compound")])
         ),
         (
-            ("LastOutputParams", "list", []),
-            ("LastOutputParams", "list", [("utags", "compound")])
-        ),
-        (
-            ("TickDelay", "int", []),
-            ("TickDelay", "int", [("utags", "compound")])
-        ),
-        (
-            ("Version", "int", []),
-            ("Version", "int", [("utags", "compound")])
-        ),
-        (
-            ("LPCommandMode", "int", []),
+            ("LPCommandMode", "int", []),  # not sure what these three are for but they seem temporary
             ("LPCommandMode", "int", [("utags", "compound")])
         ),
         (
@@ -144,7 +130,39 @@ _B113 = NBTRemapHelper(
         (
             ("LPRedstoneMode", "byte", []),
             ("LPRedstoneMode", "byte", [("utags", "compound")])
-        )
+        ),
+        (
+            ("LastExecution", "long", []),
+            ("LastExecution", "long", [("utags", "compound")])
+        ),
+        (
+            ("LastOutput", "string", []),
+            ("LastOutput", "string", [("utags", "compound")])
+        ),
+        (
+            ("LastOutputParams", "list", []),
+            ("LastOutputParams", "list", [("utags", "compound")])
+        ),
+        (
+            ("powered", "byte", []),
+            ("powered", "byte", [("utags", "compound")])
+        ),
+        (
+            ("SuccessCount", "int", []),
+            ("SuccessCount", "int", [("utags", "compound")])
+        ),
+        (
+            ("TickDelay", "int", []),
+            ("TickDelay", "int", [("utags", "compound")])
+        ),
+        (
+            ("TrackOutput", "byte", []),
+            ("TrackOutput", "byte", [("utags", "compound")])
+        ),
+        (
+            ("Version", "int", []),
+            ("Version", "int", [("utags", "compound")])
+        ),
     ],
     '{Command: "", CustomName: "", ExecuteOnFirstTick: 0b, LPCommandMode: 16064, LPCondionalMode: 63b, LPRedstoneMode: 0b, LastExecution: 0L, LastOutput: "", LastOutputParams: [], SuccessCount: 0, TickDelay: 0, TrackOutput: 1b, Version: 10, auto: 0b, conditionMet: 0b, powered: 0b}'
 )
