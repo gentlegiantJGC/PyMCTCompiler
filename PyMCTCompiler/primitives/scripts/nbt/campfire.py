71a96123d0f958976b66afb748ee5e6a504f2417
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-01-25 10:44:22 +0000

    Added a little more data for campfire and flower pot block entities

--- PyMCTCompiler/primitives/scripts/nbt/campfire.py
+++ PyMCTCompiler/primitives/scripts/nbt/campfire.py
@@ -1,6 +1,11 @@
 """
+J1.15
+{Items:[], CookingTimes[I; 0, 0, 0, 0], CookingTotalTimes[I; 0, 0, 0, 0]}
+{Slot: 0b, id:"minecraft:beef", Count: 1b}
 B1.14
 {ItemTime1: 0, ItemTime2: 0, ItemTime3: 0, ItemTime4: 0, id: "Campfire", isMovable: 1b}
+Item1-4
+{id:"minecraft:beef", Count: 1b, Damage: 0?}
 """
 
 
