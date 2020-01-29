e869802a92665d96cb7d3258206251dc286f6404
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-01-29 10:42:18 +0000

    Improved campfire and added some missing block entities to the name map

--- PyMCTCompiler/primitives/scripts/nbt/campfire.py
+++ PyMCTCompiler/primitives/scripts/nbt/campfire.py
@@ -1,3 +1,6 @@
+from PyMCTCompiler.primitives.scripts.nbt import NBTRemapHelper, EmptyNBT, merge
+from .common import java_custom_name, bedrock_is_movable, java_keep_packed
+
 """
 J1.15
 {Items:[], CookingTimes[I; 0, 0, 0, 0], CookingTotalTimes[I; 0, 0, 0, 0]}
@@ -8,12 +11,86 @@ Item1-4
 {id:"minecraft:beef", Count: 1b, Damage: 0?}
 """
 
-
 universal = {
     "nbt_identifier": ["universal_minecraft", "campfire"],
     "snbt": """{
         utags: {
-            isMovable: 1b
+            isMovable: 1b,
+            Items:[], 
+            CookingTimes: [I; 0, 0, 0, 0], 
+            CookingTotalTimes: [I; 0, 0, 0, 0]
         }
     }"""
 }
+
+_J114 = NBTRemapHelper(
+    [
+        (
+            ("Items", "list", []),
+            ("Items", "list", [("utags", "compound")])
+        ),
+        (
+            ("CookingTimes", "int_array", []),
+            ("CookingTimes", "int_array", [("utags", "compound")])
+        ),
+        (
+            ("CookingTotalTimes", "int_array", []),
+            ("CookingTotalTimes", "int_array", [("utags", "compound")])
+        )
+    ],
+    "{Items:[], CookingTimes: [I; 0, 0, 0, 0], CookingTotalTimes: [I; 0, 0, 0, 0]}"
+)
+
+_B111 = NBTRemapHelper(
+    [
+        (
+            ("ItemTime1", "int", []),
+            ("ItemTime1", "int", [("utags", "compound")])
+        ),
+        (
+            ("ItemTime2", "int", []),
+            ("ItemTime2", "int", [("utags", "compound")])
+        ),
+        (
+            ("ItemTime3", "int", []),
+            ("ItemTime3", "int", [("utags", "compound")])
+        ),
+        (
+            ("ItemTime4", "int", []),
+            ("ItemTime4", "int", [("utags", "compound")])
+        ),
+        (
+            ("Item1", "compound", []),
+            ("Item1", "compound", [("utags", "compound")])
+        ),
+        (
+            ("Item2", "compound", []),
+            ("Item2", "compound", [("utags", "compound")])
+        ),
+        (
+            ("Item3", "compound", []),
+            ("Item3", "compound", [("utags", "compound")])
+        ),
+        (
+            ("Item4", "compound", []),
+            ("Item4", "compound", [("utags", "compound")])
+        )
+    ],
+    "{ItemTime1: 0, ItemTime2: 0, ItemTime3: 0, ItemTime4: 0}"
+)
+
+j114 = merge(
+    [EmptyNBT('minecraft:campfire'), _J114, java_custom_name, java_keep_packed],
+    ['universal_minecraft:campfire']
+)
+
+b111 = merge(
+    [EmptyNBT('minecraft:campfire'), _B111, bedrock_is_movable],
+    ['universal_minecraft:campfire'],
+    abstract=True
+)
+
+b113 = merge(
+    [EmptyNBT('minecraft:campfire'), _B111, bedrock_is_movable],
+    ['universal_minecraft:campfire']
+)
