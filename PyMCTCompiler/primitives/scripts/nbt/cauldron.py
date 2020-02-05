bb3deb9a98eea2f0d7982a43390e9480ea2eb385
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2020-02-05 19:21:13 +0000

    Fixed up cauldron conversions

--- PyMCTCompiler/primitives/scripts/nbt/cauldron.py
+++ PyMCTCompiler/primitives/scripts/nbt/cauldron.py
@@ -1,3 +1,6 @@
+from PyMCTCompiler.primitives.scripts.nbt import NBTRemapHelper, EmptyNBT, merge
+from .common import bedrock_is_movable
+
 """B1.14
 {Items: [], PotionId: -1s, PotionType: 0s, id: "Cauldron", isMovable: 1b}
 """
@@ -6,7 +9,39 @@ universal = {
     "nbt_identifier": ["universal_minecraft", "cauldron"],
     "snbt": """{
         utags: {
-            isMovable: 1b
+            isMovable: 1b,
+            Items: [],
+            PotionId: -1s,
+            PotionType: 0b
         }
     }"""
 }
+
+_B110 = NBTRemapHelper(
+    [
+        (
+            ("Items", "list", []),
+            ("Items", "list", [("utags", "compound")])
+        ),
+        (
+            ("PotionId", "short", []),
+            ("PotionId", "short", [("utags", "compound")])
+        ),
+        (
+            ("PotionType", "byte", []),  # this might be wrong for old versions
+            ("PotionType", "byte", [("utags", "compound")])
+        )
+    ],
+    "{Items:[], PotionId: -1s, PotionType: 0b}"
+)
+
+b110 = merge(
+    [EmptyNBT('minecraft:cauldron'), _B110, bedrock_is_movable],
+    ['universal_minecraft:cauldron'],
+    abstract=True
+)
+
+b113 = merge(
+    [EmptyNBT('minecraft:cauldron'), _B110, bedrock_is_movable],
+    ['universal_minecraft:cauldron']
+)
