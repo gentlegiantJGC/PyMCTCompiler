f88a2a84bc1a91071a649ed4ecd60c602f163189
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-03-26 14:00:45 +0000

    Added the metadata for B1.16

---
+++ PyMCTCompiler/version_compiler/bedrock_1_16_0/__init__.py
@@ -0,0 +1,8 @@
+from PyMCTCompiler.compilers.nbt_blockstate_compiler import NBTBlockstateCompiler
+import os
+
+compiler = NBTBlockstateCompiler(
+    os.path.dirname(__file__),
+    version=[1, 16, 0],
+    parent_version='bedrock_1_14_0'
+)
