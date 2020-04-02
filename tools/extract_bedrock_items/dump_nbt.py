b344c4f908e8765b3a1f935de8da82ea5a9dbc2c
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-04-02 16:43:35 +0100

    Added programs to extract item data from Bedrock

---
+++ tools/extract_bedrock_items/dump_nbt.py
@@ -0,0 +1,49 @@
+"""This is a plugin for Amulet that will export item NBT for a given selection in SNBT format"""
+
+from typing import TYPE_CHECKING
+
+from amulet.api.selection import Selection
+import os
+import copy
+
+if TYPE_CHECKING:
+    from amulet.api.world import World
+
+snbt_dir = r"."  # the location to export the snbt to
+
+
+def snbt(
+    world: "World",
+    dimension: int,
+    selection_box: Selection
+):
+    blocks = set()
+    items = set()
+    for chunk, slices, box in world.get_chunk_slices(selection_box, dimension):
+        for be in chunk.block_entities:
+            if be.location in box:
+                for item in be.nbt['utags']['Items']:
+                    item = copy.deepcopy(item)
+                    if 'WasPickedUp' in item:
+                        del item['WasPickedUp']
+                    if 'Count' in item:
+                        del item['Count']
+                    if 'Slot' in item:
+                        del item['Slot']
+                    if 'Block' in item:
+                        blocks.add(item.to_snbt())
+                    else:
+                        items.add(item.to_snbt())
+    with open(os.path.join(snbt_dir, 'items.txt'), 'w') as f:
+        f.write('\n'.join(sorted(items)))
+    with open(os.path.join(snbt_dir, 'blocks.txt'), 'w') as f:
+        f.write('\n'.join(sorted(blocks)))
+
+
+export = {
+    "v": 1,  # a version 1 plugin
+    "name": "Dump NBT",  # the name of the plugin
+    "features": ["src_selection"],
+    "inputs": ["src_selection"],  # the inputs to give to the plugin
+    "operation": snbt,  # the actual function to call when running the plugin
+}
---
+++ tools/extract_bedrock_items/mcb_strings.py
@@ -0,0 +1,74 @@
+import subprocess
+import re
+import os
+import glob
+
+mc_exe = glob.glob(r"C:\Program Files\WindowsApps\Microsoft.MinecraftUWP*\Minecraft.Windows.exe")
+if mc_exe:
+    mc_exe = mc_exe[0]
+else:
+    raise Exception('Could not find exe')
+zip_path = r"D:\Programs\7-Zip\7z.exe"  # this will need setting based on your install
+pack = os.path.join(os.getenv('LOCALAPPDATA'), r"Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs\items_bp\functions")
+
+match = re.compile(b'[_a-zA-Z0-9]+')
+namespaced_match = re.compile(b'minecraft:[_a-zA-Z0-9]+')
+
+subprocess.run([zip_path, 'e', mc_exe, '.rdata'])
+
+strings = set()
+namespaced_strings = set()
+
+with open('.rdata', 'rb') as f:
+    fsplit = f.read().split(b'\x00')
+    for s in fsplit:
+        if s:
+            if namespaced_match.fullmatch(s):
+                namespaced_strings.add(s)
+            elif match.fullmatch(s):
+                strings.add(s)
+
+main_commands = []
+commands = []
+slot = 0
+x = 0
+z = 0
+
+for s in strings:
+    commands.append(f"spawnitem {s.decode('utf-8')} ~{x} ~ ~{z}")
+    slot += 1
+    if slot == 150:
+        with open(os.path.join(pack, f'items/chest{x}_{z}.mcfunction'), 'w') as f:
+            f.write('\n'.join(commands))
+        commands.clear()
+        main_commands.append(f'execute @a[scores={{t={x*32+z}}}] ~ ~ ~ function items/chest{x}_{z}')
+        slot = 0
+        z += 1
+    if z == 32:
+        z = 0
+        x += 1
+
+main_commands.append('scoreboard players add @a t 1')
+
+with open(os.path.join(pack, 'items/setup.mcfunction'), 'w') as f:
+    f.write('\n'.join(
+        [
+            'scoreboard objectives add t dummy',
+            'scoreboard players set @a t 10000',
+            f'fill ~-1 ~-2 ~-1 ~{x+1} ~10 ~33 barrier 0 outline',
+            f'fill ~ ~-2 ~ ~{x} ~-1 ~32 chest',
+            f'fill ~ ~-1 ~ ~{x} ~-1 ~32 hopper',
+            'give @s repeating_command_block'
+        ]
+    ))
+
+with open(os.path.join(pack, 'items/cleanup.mcfunction'), 'w') as f:
+    f.write(f'fill ~-1 ~-2 ~-1 ~{x+1} ~10 ~33 air')
+
+with open(os.path.join(pack, 'items/loop.mcfunction'), 'w') as f:
+    f.write('\n'.join(main_commands))
+
+with open(os.path.join(pack, 'items/summon.mcfunction'), 'w') as f:
+    f.write(
+        'scoreboard players set @s t 0'
+    )
