5273bab18ccaa3f43520d0029094edf2e3ced54e
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-05-05 12:27:10 +0100

    added universal nbt entry for beehives

--- PyMCTCompiler/primitives/scripts/nbt/beehive.py
+++ PyMCTCompiler/primitives/scripts/nbt/beehive.py
@@ -2,6 +2,15 @@
 from PyMCTCompiler.primitives.scripts.nbt import EmptyNBT, merge, NBTRemapHelper
 from .common import bedrock_is_movable, java_keep_packed
 
+universal = {
+    "nbt_identifier": ["universal_minecraft", "beehive"],
+    "snbt": """{
+        utags: {
+            isMovable: 1b
+        }
+    }"""
+}
+
 j115 = merge(
     [EmptyNBT('minecraft:beehive'), java_keep_packed],
     ['universal_minecraft:beehive']
