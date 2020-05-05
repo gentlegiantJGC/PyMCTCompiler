02f1a769cc567cbb825f29bcd3a72ab107744d05
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-05-05 12:04:48 +0100

    Fixed block entities
    Added some missing block entities.

---
+++ PyMCTCompiler/primitives/scripts/nbt/beehive.py
@@ -0,0 +1,23 @@
+# TODO
+from PyMCTCompiler.primitives.scripts.nbt import EmptyNBT, merge, NBTRemapHelper
+from .common import bedrock_is_movable, java_keep_packed
+
+j115 = merge(
+    [EmptyNBT('minecraft:beehive'), java_keep_packed],
+    ['universal_minecraft:beehive']
+)
+
+j115_nest = merge(
+    [EmptyNBT('minecraft:beehive'), java_keep_packed],
+    ['universal_minecraft:bee_nest']
+)
+
+b114 = merge(
+    [EmptyNBT('minecraft:beehive'), bedrock_is_movable],
+    ['universal_minecraft:beehive']
+)
+
+b114_nest = merge(
+    [EmptyNBT('minecraft:beehive'), bedrock_is_movable],
+    ['universal_minecraft:bee_nest']
+)
