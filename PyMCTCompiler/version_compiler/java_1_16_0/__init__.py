17ef44f4cde01ea9b4434f14d54ca71ec4cc56e1
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-06-23 15:04:08 +0100

    Added J1.16 version

---
+++ PyMCTCompiler/version_compiler/java_1_16_0/__init__.py
@@ -0,0 +1,9 @@
+from PyMCTCompiler.compilers.java_blockstate_compiler import JavaBlockstateCompiler
+import os
+
+compiler = JavaBlockstateCompiler(
+    os.path.dirname(__file__),
+    version=[1, 16, 0],
+    parent_version='java_1_15_2',
+    data_version=2566
+)
