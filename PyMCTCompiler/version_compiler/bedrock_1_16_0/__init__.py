f526fbb9c4fd1d0224bfb4aa335e24300b031758
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-06-24 14:12:20 +0100

    Fixed some Bedrock block ids

--- PyMCTCompiler/version_compiler/bedrock_1_16_0/__init__.py
+++ PyMCTCompiler/version_compiler/bedrock_1_16_0/__init__.py
@@ -5,5 +5,5 @@ compiler = NBTBlockstateCompiler(
     os.path.dirname(__file__),
     version=[1, 16, 0],
     parent_version='bedrock_1_14_0',
-    data_version=17825792
+    data_version=17825806
 )
