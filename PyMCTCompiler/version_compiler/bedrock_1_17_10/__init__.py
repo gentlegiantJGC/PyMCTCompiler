a6e106cfbf12c3c7deaf600b1723f9887d1ab57e
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-09-23 12:02:44 +0100

    Added the newer Bedrock 1.17 versions

---
+++ PyMCTCompiler/version_compiler/bedrock_1_17_10/__init__.py
@@ -0,0 +1,9 @@
+from PyMCTCompiler.compilers.nbt_blockstate_compiler import NBTBlockstateCompiler
+import os
+
+compiler = NBTBlockstateCompiler(
+    os.path.dirname(__file__),
+    version=[1, 17, 10],
+    parent_version='bedrock_1_17_0',
+    data_version=17879555
+)
