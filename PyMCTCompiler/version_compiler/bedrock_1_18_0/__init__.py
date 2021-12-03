a988db4087725f30d1c088cb629e69ada0cee079
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-12-03 13:03:56 +0000

    Added 1.18 versions

---
+++ PyMCTCompiler/version_compiler/bedrock_1_18_0/__init__.py
@@ -0,0 +1,9 @@
+from PyMCTCompiler.compilers.nbt_blockstate_compiler import NBTBlockstateCompiler
+import os
+
+compiler = NBTBlockstateCompiler(
+    os.path.dirname(__file__),
+    version=[1, 18, 0],
+    parent_version='bedrock_1_17_30',
+    data_version=17879555
+)
---
+++ PyMCTCompiler/version_compiler/java_1_18_0/__init__.py
@@ -0,0 +1,9 @@
+from PyMCTCompiler.compilers.java_blockstate_compiler import JavaBlockstateCompiler
+import os
+
+compiler = JavaBlockstateCompiler(
+    os.path.dirname(__file__),
+    version=[1, 18, 0],
+    parent_version='java_1_17_0',
+    data_version=2860
+)
