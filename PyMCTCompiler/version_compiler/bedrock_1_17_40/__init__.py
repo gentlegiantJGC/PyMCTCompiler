d900d68bb31e7ee06b2cea3394e00a11e9bdf16f
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-12-03 15:01:05 +0000

    Added some more versions to support biome changes

--- PyMCTCompiler/version_compiler/bedrock_1_17_40/__init__.py
+++ PyMCTCompiler/version_compiler/bedrock_1_17_40/__init__.py
@@ -3,7 +3,7 @@ import os
 
 compiler = NBTBlockstateCompiler(
     os.path.dirname(__file__),
-    version=[1, 17, 0],
-    parent_version='bedrock_1_16_220',
+    version=[1, 17, 40],
+    parent_version='bedrock_1_17_30',
     data_version=17879555
 )
