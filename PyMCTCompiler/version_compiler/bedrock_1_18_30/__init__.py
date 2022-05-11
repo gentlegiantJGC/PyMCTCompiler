2391bbc6d8f121e87d3fc5fa70ab353943d80906
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-05-11 13:13:23 +0100

    Added Bedrock 1.18.30

---
+++ PyMCTCompiler/version_compiler/bedrock_1_18_30/__init__.py
@@ -0,0 +1,9 @@
+from PyMCTCompiler.compilers.nbt_blockstate_compiler import NBTBlockstateCompiler
+import os
+
+compiler = NBTBlockstateCompiler(
+    os.path.dirname(__file__),
+    version=[1, 18, 30],
+    parent_version='bedrock_1_18_0',
+    data_version=17959425
+)
