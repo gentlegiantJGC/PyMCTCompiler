f02cf474be6732b8f243cc35c1485458dc2c21dc
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-06-05 13:30:56 +0100

    Added Java 1.19

---
+++ PyMCTCompiler/version_compiler/java_1_19_0/__init__.py
@@ -0,0 +1,9 @@
+from PyMCTCompiler.compilers.java_blockstate_compiler import JavaBlockstateCompiler
+import os
+
+compiler = JavaBlockstateCompiler(
+    os.path.dirname(__file__),
+    version=[1, 19, 0],
+    parent_version='java_1_18_0',
+    data_version=3104
+)
