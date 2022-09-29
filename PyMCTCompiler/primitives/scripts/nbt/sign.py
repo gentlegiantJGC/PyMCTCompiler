beb34cab08f788ab34ec6d89f8957c4f69b4f73c
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-09-29 15:32:15 +0100

    Updated signs

--- PyMCTCompiler/primitives/scripts/nbt/sign.py
+++ PyMCTCompiler/primitives/scripts/nbt/sign.py
@@ -6,6 +6,14 @@ Default
 J113    "minecraft:sign"		'{Text4: "{\\"text\\":\\"\\"}", Text3: "{\\"text\\":\\"\\"}", Text2: "{\\"text\\":\\"\\"}", Text1: "{\\"text\\":\\"\\"}"}'
 
 B113	"Sign"		"{Text: "", TextOwner: "", isMovable: 1b}"
+B116?   {
+    IgnoreLighting: 0b,                 1 when the glow effect is applied
+    PersistFormatting: 1b, 
+    SignTextColor: -16777216,           BGRA
+    Text: "",                           The text on the sign
+    TextIgnoreLegacyBugResolved: 0b,    0 when initially placed, 1 after first time glow is applied. https://bugs.mojang.com/browse/MCPE-117835
+    TextOwner: ""                       player xuid
+}
 """
 
 universal = {
@@ -75,6 +83,28 @@ _BText = TranslationFile(
     }
 )
 
+_B116 = NBTRemapHelper(
+    [
+        (
+            ("IgnoreLighting", "byte", []),
+            ("IgnoreLighting", "byte", [("utags", "compound")])
+        ),
+        (
+            ("PersistFormatting", "byte", []),
+            ("PersistFormatting", "byte", [("utags", "compound")])
+        ),
+        (
+            ("SignTextColor", "int", []),
+            ("SignTextColor", "int", [("utags", "compound")])
+        ),
+        (
+            ("TextIgnoreLegacyBugResolved", "byte", []),
+            ("TextIgnoreLegacyBugResolved", "byte", [("utags", "compound")])
+        )
+    ],
+    '{IgnoreLighting: 0b, PersistFormatting: 1b, SignTextColor: -16777216, TextIgnoreLegacyBugResolved: 0b}'
+)
+
 j19 = merge(
     [EmptyNBT('minecraft:sign'), _J19],
     ['universal_minecraft:sign'],
@@ -116,5 +146,15 @@ b113 = merge(
 
 wall_b113 = merge(
     [EmptyNBT(':Sign'), _B17, _BText, bedrock_is_movable],
+    ['universal_minecraft:wall_sign']
+)
+
+b116 = merge(
+    [EmptyNBT(':Sign'), _B17, _B116, _BText, bedrock_is_movable],
+    ['universal_minecraft:sign']
+)
+
+wall_b116 = merge(
+    [EmptyNBT(':Sign'), _B17, _B116, _BText, bedrock_is_movable],
     ['universal_minecraft:wall_sign']
 )
