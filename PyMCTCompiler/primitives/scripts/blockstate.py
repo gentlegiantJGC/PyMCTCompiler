3ec375a2179b10590e9843e3e03e055df6372817
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-04-20 13:55:22 +0100

    Added cherry blocks and other Java 1.20 blocks

--- PyMCTCompiler/primitives/scripts/blockstate.py
+++ PyMCTCompiler/primitives/scripts/blockstate.py
@@ -1136,3 +1136,124 @@ def torch(input_namespace: str, input_block_name: str, wall: bool, default_block
 					]
 				}
 			}
+
+def sign193(
+	input_namespace: str,
+	input_block_name: str,
+	material: str,
+	hanging: bool,
+	universal_namespace: str = None,
+	universal_block_name: str = None,
+	carry_properties: Dict[str, List[str]] = None,
+	default: str = ''
+) -> dict:
+	if universal_namespace is None:
+		universal_namespace = input_namespace
+	if universal_block_name is None:
+		universal_block_name = input_block_name
+
+	tu = [
+		{
+			"function": "new_block",
+			"options": f"{universal_namespace}:{universal_block_name}"
+		},
+		{
+			"function": "new_properties",
+			"options": {
+				**{"material": material},
+				**({} if hanging else {"connection": "\"down\""})
+			}
+		}
+	] + [
+		{
+			"function": "map_properties",
+			"options": {
+				"attached": {
+					"\"false\"": [
+						{
+							"function": "new_properties",
+							"options": {"connection": "\"up\""}
+						}
+					],
+					"\"true\"": [
+						{
+							"function": "new_properties",
+							"options": {"connection": "\"up_chain\""}
+						}
+					]
+				}
+			}
+		}
+	] * hanging
+	fu = [
+		{
+			"function": "map_properties",
+			"options": {
+				"material": {
+					material: [
+						{
+							"function": "map_properties",
+							"options": {
+								"connection": {
+									"\"up\"": [
+										{
+											"function": "new_block",
+											"options": f"{input_namespace}:{input_block_name}"
+										},
+										{
+											"function": "new_properties",
+											"options": {
+												"attached": "\"false\""
+											}
+										}
+									],
+									"\"up_chain\"": [
+										{
+											"function": "new_block",
+											"options": f"{input_namespace}:{input_block_name}"
+										},
+										{
+											"function": "new_properties",
+											"options": {
+												"attached": "\"true\""
+											}
+										}
+									]
+								} if hanging else {
+									"\"down\"": [
+										{
+											"function": "new_block",
+											"options": f"{input_namespace}:{input_block_name}"
+										}
+									]
+								}
+							}
+						}
+					]
+				}
+			}
+		}
+	]
+	if default:
+		fu.insert(
+			0,
+			{
+				"function": "new_block",
+				"options": default
+			}
+		)
+	if carry_properties is not None:
+		tu.append({
+			"function": "carry_properties",
+			"options": carry_properties
+		})
+		fu.append({
+			"function": "carry_properties",
+			"options": carry_properties
+		})
+	return {
+		"to_universal": tu,
+		"from_universal": {
+			f"{universal_namespace}:{universal_block_name}": fu
+		}
+	}
---
+++ PyMCTCompiler/version_compiler/java_1_19_4/__init__.py
@@ -0,0 +1,9 @@
+from PyMCTCompiler.compilers.java_blockstate_compiler import JavaBlockstateCompiler
+import os
+
+compiler = JavaBlockstateCompiler(
+    os.path.dirname(__file__),
+    version=[1, 19, 4],
+    parent_version='java_1_19_3',
+    data_version=3337
+)
