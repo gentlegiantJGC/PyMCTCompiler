dcb182dd85380a65a86bfaeb4fd50fd3fbb04639
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-04-23 14:06:34 +0100

    Fixed Java wall signs

--- PyMCTCompiler/primitives/scripts/blockstate.py
+++ PyMCTCompiler/primitives/scripts/blockstate.py
@@ -1257,3 +1257,80 @@ def sign193(
 			f"{universal_namespace}:{universal_block_name}": fu
 		}
 	}
+
+
+def wall_sign193(
+	input_namespace: str,
+	input_block_name: str,
+	material: str,
+	hanging: bool,
+	universal_namespace: str = None,
+	universal_block_name: str = None,
+	carry_properties: Dict[str, List[str]] = None,
+	default: str = ''
+) -> dict:
+	if universal_namespace is None:
+		universal_namespace = input_namespace
+	if universal_block_name is None:
+		universal_block_name = input_block_name
+
+	tu = [
+		{
+			"function": "new_block",
+			"options": f"{universal_namespace}:{universal_block_name}"
+		},
+		{
+			"function": "new_properties",
+			"options": {
+				"material": material,
+				"hanging": ["\"false\"", "\"true\""][hanging]
+			}
+		}
+	]
+	fu = [
+		{
+			"function": "map_properties",
+			"options": {
+				"material": {
+					material: [
+						{
+							"function": "map_properties",
+							"options": {
+								"hanging": {
+									"\"true\"" if hanging else "\"false\"": [
+										{
+											"function": "new_block",
+											"options": f"{input_namespace}:{input_block_name}"
+										}
+									]
+								}
+							}
+						}
+					]
+				}
+			}
+		}
+	]
+	if default:
+		fu.insert(
+			0,
+			{
+				"function": "new_block",
+				"options": default
+			}
+		)
+	if carry_properties is not None:
+		tu.append({
+			"function": "carry_properties",
+			"options": carry_properties
+		})
+		fu.append({
+			"function": "carry_properties",
+			"options": carry_properties
+		})
+	return {
+		"to_universal": tu,
+		"from_universal": {
+			f"{universal_namespace}:{universal_block_name}": fu
+		}
+	}
