37bd875861f1e727b8f1b2c111379e945f47c914
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-06-06 10:42:13 +0100

    Added real block palettes

---
+++ PyMCTCompiler/version_compiler/bedrock_1_19_80/__init__.py
@@ -0,0 +1,9 @@
+from PyMCTCompiler.compilers.nbt_blockstate_compiler import NBTBlockstateCompiler
+import os
+
+compiler = NBTBlockstateCompiler(
+    os.path.dirname(__file__),
+    version=[1, 19, 80],
+    parent_version='bedrock_1_19_70',
+    data_version=18040335
+)
