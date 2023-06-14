8344b6281e9029af2f83c8ef0a7dda13c43782f0
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-06-14 15:21:01 +0100

    Added decorated pot

---
+++ PyMCTCompiler/primitives/scripts/nbt/decorated_pot.py
@@ -0,0 +1,28 @@
+from PyMCTCompiler.primitives.scripts.nbt import EmptyNBT, merge
+from .common import bedrock_is_movable
+
+"""
+Default
+J113    "minecraft:decorated_pot"		"{sherds: ["minecraft:brick", "minecraft:brick", "minecraft:brick", "minecraft:brick"]}"
+
+B120	"DecoratedPot"		CompoundTag({'isMovable': ByteTag(1), 'sherds': ListTag([StringTag(""), StringTag(""), StringTag(""), StringTag("")], 8)})
+"""
+
+universal = {
+    "nbt_identifier": ["universal_minecraft", "decorated_pot"],
+    "snbt": """{
+        utags: {
+            isMovable: 1b
+        }
+    }"""
+}
+
+j120 = merge(
+    [EmptyNBT('minecraft:decorated_pot')],
+    ['universal_minecraft:decorated_pot']
+)
+
+b120 = merge(
+    [EmptyNBT(':DecoratedPot'), bedrock_is_movable],
+    ['universal_minecraft:decorated_pot']
+)
