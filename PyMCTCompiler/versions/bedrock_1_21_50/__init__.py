b505df2e8cf49df91cc6da7460391d005e891de7
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-12-04 18:11:42 +0000

    Added B1.21.50

---
+++ PyMCTCompiler/versions/bedrock_1_21_50/__init__.py
@@ -0,0 +1,11 @@
+from PyMCTCompiler.compilers.nbt_blockstate_compiler import NBTBlockstateCompiler
+import os
+
+compiler = NBTBlockstateCompiler(
+    os.path.dirname(__file__),
+    version=[1, 21, 50],
+    version_max=[2, -1],
+    parent_version="bedrock_1_21_40",
+    data_version=18163714,
+    data_version_max=9999999999,
+)
