bb4d2d7bcf76e1fad45fa47dfa6c2c5a3fd18489
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-03-24 08:58:27 +0000

    Fix max versions

--- PyMCTCompiler/versions/bedrock_1_21_20/__init__.py
+++ PyMCTCompiler/versions/bedrock_1_21_20/__init__.py
@@ -4,7 +4,7 @@ import os
 compiler = NBTBlockstateCompiler(
     os.path.dirname(__file__),
     version=[1, 21, 20],
-    version_max=[2, 21, 40, -1],
+    version_max=[1, 21, 40, -1],
     parent_version="bedrock_1_21_0",
     data_version=18158598,
     data_version_max=18163712,
--- PyMCTCompiler/versions/bedrock_1_21_40/__init__.py
+++ PyMCTCompiler/versions/bedrock_1_21_40/__init__.py
@@ -4,7 +4,7 @@ import os
 compiler = NBTBlockstateCompiler(
     os.path.dirname(__file__),
     version=[1, 21, 40],
-    version_max=[2, -1],
+    version_max=[1, 21, 50, -1],
     parent_version="bedrock_1_21_20",
     data_version=18163713,
     data_version_max=18163713,
