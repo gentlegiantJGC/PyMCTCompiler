0db72ddd03be4357eb5f730f089a165017bf6a59
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-07-01 07:49:43 +0100

    Bedrock 1.21.80 (#20)
    * Add Bedrock 1.21.80
    
    * Add dried ghast translation

--- PyMCTCompiler/versions/bedrock_1_21_80/__init__.py
+++ PyMCTCompiler/versions/bedrock_1_21_80/__init__.py
@@ -3,9 +3,9 @@ import os
 
 compiler = NBTBlockstateCompiler(
     os.path.dirname(__file__),
-    version=[1, 21, 70],
-    version_max=[1, 21, 80, -1],
-    parent_version="bedrock_1_21_60",
+    version=[1, 21, 80],
+    version_max=[2, -1],
+    parent_version="bedrock_1_21_70",
     data_version=18168865,
-    data_version_max=18168865,
+    data_version_max=9999999999,
 )
