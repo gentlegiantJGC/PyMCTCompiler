b16d73d23f34ecaa35bc2eabbfa91b2643207e87
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-10-08 16:16:19 +0100

    Add Java 1.21.10

--- PyMCTCompiler/versions/java_1_21_10/__init__.py
+++ PyMCTCompiler/versions/java_1_21_10/__init__.py
@@ -3,9 +3,9 @@ import os
 
 compiler = JavaBlockstateCompiler(
     os.path.dirname(__file__),
-    version=[1, 21, 9],
-    version_max=[1, 21, 10, -1],
-    parent_version="java_1_21_8",
-    data_version=4553,
-    data_version_max=4554,
+    version=[1, 21, 10],
+    version_max=[2, -1],
+    parent_version="java_1_21_9",
+    data_version=4555,
+    data_version_max=99999999,
 )
