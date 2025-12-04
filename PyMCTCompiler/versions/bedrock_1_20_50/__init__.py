8e3eb9142f195737be94bf09d0cea141a46faa7e
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-12-04 15:56:08 +0000

    Remove Java 1.21.10 and rename Bedrock versions (#24)
    Java 1.21.10 is the same as 1.21.9.

--- PyMCTCompiler/versions/bedrock_1_20_50/__init__.py
+++ PyMCTCompiler/versions/bedrock_1_20_50/__init__.py
@@ -5,7 +5,7 @@ compiler = NBTBlockstateCompiler(
     os.path.dirname(__file__),
     version=[1, 20, 50],
     version_max_known=[1, 20, 51],
-    version_max=[1, 20, 61, -1],
+    version_max=[1, 20, 60, -1],
     parent_version="bedrock_1_20_40",
     data_version=18100737,
     data_version_max_known=18100737,
